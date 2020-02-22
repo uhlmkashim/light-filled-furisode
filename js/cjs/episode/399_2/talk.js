e767e63c7085af457fc36f6cb7e1b6c2b052186d
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-02-23 04:56:32 +0900

    Create talk.js

---
+++ js/cjs/episode/399_2/talk.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "菜の花が､とても綺麗ですね｡";
+window.im_cjs.t[1][2] = "この辺りは､もうすっかり春が訪";
+window.im_cjs.t[1][3] = "れているみたい……｡随分と､";
+window.im_cjs.t[1][4] = "南の方までやってきましたね｡";
+
+window.im_cjs.t[2][1] = "飛行機でひとっ飛びもいいです";
+window.im_cjs.t[2][2] = "けど……ｶﾞﾀﾝ､ｺﾞﾄﾝと揺られて､";
+window.im_cjs.t[2][3] = "流れる景色をゆっくり楽しめる";
+window.im_cjs.t[2][4] = "のは､列車の旅ならでは｡";
+
+window.im_cjs.t[3][1] = "そこに､ご当地名物の駅弁と､冷";
+window.im_cjs.t[3][2] = "えたﾋﾞｰﾙ｡青い空と､満開の菜の";
+window.im_cjs.t[3][3] = "花｡そして､ﾌﾟﾛﾃﾞｭｰｻｰとの楽しい";
+window.im_cjs.t[3][4] = "会話も｡本当に贅沢……｡";
+
+window.im_cjs.t[4][1] = "そんな贅沢な時間を乗せて､列";
+window.im_cjs.t[4][2] = "車は私たちをどこへ連れて行っ";
+window.im_cjs.t[4][3] = "てくれるんでしょう……なんて｡";
+window.im_cjs.t[4][4] = "目的地は､わかってます｡";
+
+window.im_cjs.t[5][1] = "温泉､とっても楽しみなんです｡";
+window.im_cjs.t[5][2] = "ﾌﾟﾛﾃﾞｭｰｻｰがｵｽｽﾒしてくれた通";
+window.im_cjs.t[5][3] = "り､お湯に浸かってﾘﾗｯｸｽする";
+window.im_cjs.t[5][4] = "のって､私､大好きですから｡";
+
+window.im_cjs.t[6][1] = "でも､到着までまだ暫くかかり";
+window.im_cjs.t[6][2] = "ますし……まずは目の前のお弁";
+window.im_cjs.t[6][3] = "当とお酒で､心のｺﾘをほぐしま";
+window.im_cjs.t[6][4] = "しょうか｡さあ､もう一杯｡";
+
+window.im_cjs.t[7][1] = "それとも､ﾃﾞｻﾞｰﾄをお望みです";
+window.im_cjs.t[7][2] = "か? でしたら､おみかんをどう";
+window.im_cjs.t[7][3] = "ぞ｡今日は私も……";
+window.im_cjs.t[7][4] = "ﾌﾟﾛﾃﾞｭｰｻｰと､";
+window.im_cjs.t[7][5] = "ゆっくり楽しみますね♪";
