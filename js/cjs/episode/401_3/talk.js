76796e9d1dfeaad5674adaa9c761169d429aa09f
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-02-29 16:53:36 +0900

    Create talk.js

---
+++ js/cjs/episode/401_3/talk.js
@@ -0,0 +1,41 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "お待たせしました～っ､特製･ﾅﾅ";
+window.im_cjs.t[1][2] = "のﾒﾙﾍﾝ弁当です! ふわふわの卵";
+window.im_cjs.t[1][3] = "焼きにｻｸｻｸの唐揚げ､おにぎり";
+window.im_cjs.t[1][4] = "やﾎﾟﾃｻﾗもありますよ～!";
+
+window.im_cjs.t[2][1] = "そして､極めつけは……ﾒﾙﾍﾝ･ｳｻ";
+window.im_cjs.t[2][2] = "ﾐﾝｳｨﾝﾅｰ☆やっぱり､お弁当には";
+window.im_cjs.t[2][3] = "これですよね! ささっ､どうぞ! ";
+window.im_cjs.t[2][4] = "たーんと召し上がれ♪";
+
+window.im_cjs.t[3][1] = "あれれ? ﾅﾅの料理ｽｷﾙに驚いて";
+window.im_cjs.t[3][2] = "ません? このくらいできます";
+window.im_cjs.t[3][3] = "よ～｡伊達に年を重ねて……い";
+window.im_cjs.t[3][4] = "えっ､立派なｳｻﾐﾐﾒｲﾄﾞですから!";
+
+window.im_cjs.t[4][1] = "な～んて｡かなり早起きして";
+window.im_cjs.t[4][2] = "作ったんですけどね……えへへ｡";
+window.im_cjs.t[4][3] = "ﾌﾟﾛﾃﾞｭｰｻｰさんに喜んでほしく";
+window.im_cjs.t[4][4] = "て､頑張っちゃいました♪";
+
+window.im_cjs.t[5][1] = "ﾌﾟﾛﾃﾞｭｰｻｰさんはいつも､ﾅﾅのた";
+window.im_cjs.t[5][2] = "めに一生懸命ですから｡今日は";
+window.im_cjs.t[5][3] = "たくさん栄養をつけて､の～ん";
+window.im_cjs.t[5][4] = "びり癒されてほしいんです｡";
+
+window.im_cjs.t[6][1] = "だから今日は､お弁当を食べた";
+window.im_cjs.t[6][2] = "ら､ごろ～んと寝そべって､日向";
+window.im_cjs.t[6][3] = "ぼっこもしちゃいましょう♪";
+window.im_cjs.t[6][4] = "ぐーたら､大歓迎ですよっ!";
+
+window.im_cjs.t[7][1] = "決まりですね! じゃあ､まずは";
+window.im_cjs.t[7][2] = "お弁当にしましょう｡一口目は､";
+window.im_cjs.t[7][3] = "ﾅﾅが食べさせてあげますねっ! ";
+window.im_cjs.t[7][4] = "お口をあけて……あ～ん♪";
