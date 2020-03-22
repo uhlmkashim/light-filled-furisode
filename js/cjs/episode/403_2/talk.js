c0eeaf6faa98b2e955516efa780f6cea32372d81
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-03-22 19:05:02 +0900

    Create talk.js

---
+++ js/cjs/episode/403_2/talk.js
@@ -0,0 +1,43 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "ﾋﾟﾝﾎﾟ～ﾝ";
+
+window.im_cjs.t[2][1] = "はいはい､開けるよー♪お待た";
+window.im_cjs.t[2][2] = "せ～……って､なんだｷﾐかー｡";
+window.im_cjs.t[2][3] = "ちょうど今､いいとこなんだよ";
+window.im_cjs.t[2][4] = "ね｡Shhh,be quiet.Okay?";
+
+window.im_cjs.t[3][1] = "うん､よろしい♪で､ｷﾐは何をし";
+window.im_cjs.t[3][2] = "に来たのかなー? 女の子の園に､";
+window.im_cjs.t[3][3] = "まさか何も用意せずやってきた";
+window.im_cjs.t[3][4] = "ﾜｹじゃないよねぇ?";
+
+window.im_cjs.t[4][1] = "もったいぶらずに差し出してご";
+window.im_cjs.t[4][2] = "らんよ｡ｷﾐの後ろ､隠してるんで";
+window.im_cjs.t[4][3] = "しょ? 貪欲な女の子たちが望ん";
+window.im_cjs.t[4][4] = "でる､大好きなｱﾚがさ｡";
+
+window.im_cjs.t[5][1] = "隠したってわかるよー｡その匂";
+window.im_cjs.t[5][2] = "いは､上映会をよりｽﾃｷな何かに";
+window.im_cjs.t[5][3] = "変えてくれるﾓﾉ……例えるなら､";
+window.im_cjs.t[5][4] = "ﾁｰｽﾞとﾄﾏﾄの匂い｡";
+
+window.im_cjs.t[6][1] = "つまり､ｷﾐの背中に隠れてるの";
+window.im_cjs.t[6][2] = "は……注文した超特大･ﾃﾞﾗｯｸｽ";
+window.im_cjs.t[6][3] = "ｽﾍﾟｼｬﾘﾃｨｰﾋﾟｻﾞと見た! おっ､";
+window.im_cjs.t[6][4] = "正解! んはー､美味しそー♪";
+
+window.im_cjs.t[7][1] = "買ってきてくれて､ありがと! ";
+window.im_cjs.t[7][2] = "……っとと､いけない｡大きな声";
+window.im_cjs.t[7][3] = "出しちゃった｡興奮しちゃって､";
+window.im_cjs.t[7][4] = "つい♪静かにね､静かに｡";
+
+window.im_cjs.t[8][1] = "でもさー､やっぱ映画にはｼﾞｬﾝｸ";
+window.im_cjs.t[8][2] = "なﾌｰﾄﾞだよ｡……うん､ｷﾐの参加";
+window.im_cjs.t[8][3] = "を許可しよう! さぁ､お入りよ｡";
+window.im_cjs.t[8][4] = "秘密のｼｱﾀｰへようこそ♪";
