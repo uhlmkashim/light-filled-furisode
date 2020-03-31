3510c4020f230e65cf2cce99db440a5042caf6bf
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-03-31 18:21:50 +0900

    Create talk.js

---
+++ js/cjs/episode/404_4/talk.js
@@ -0,0 +1,41 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "あの……ﾌﾟﾛﾃﾞｭｰｻｰさん? そん";
+window.im_cjs.t[1][2] = "なに見つめていただかなくても､";
+window.im_cjs.t[1][3] = "大丈夫です｡そばにいますし……";
+window.im_cjs.t[1][4] = "迷子にはなりませんから｡";
+
+window.im_cjs.t[2][1] = "それとも……桜の美しさに､捕";
+window.im_cjs.t[2][2] = "らわれてしまいましたか? それ";
+window.im_cjs.t[2][3] = "なら……私も､同じです｡美しく､";
+window.im_cjs.t[2][4] = "見事に咲いていますね｡";
+
+window.im_cjs.t[3][1] = "ここに来るまで……少し､心配";
+window.im_cjs.t[3][2] = "していたんです｡人がたくさん";
+window.im_cjs.t[3][3] = "集まる場所だから､ひっそりと";
+window.im_cjs.t[3][4] = "楽しめるか……｡";
+
+window.im_cjs.t[4][1] = "けど……杞憂だったみたいです";
+window.im_cjs.t[4][2] = "ね｡みなさん､桜に見惚れていて";
+window.im_cjs.t[4][3] = "……きっと誰も､私たちには気";
+window.im_cjs.t[4][4] = "づいていませんから｡";
+
+window.im_cjs.t[5][1] = "だから､今日は……ちょっぴり";
+window.im_cjs.t[5][2] = "お酒も楽しんじゃおうかなって｡";
+window.im_cjs.t[5][3] = "ふふっ､見てください｡苺のｼｬﾝﾊﾟ";
+window.im_cjs.t[5][4] = "ﾝ､可愛いですよね……♪";
+
+window.im_cjs.t[6][1] = "……実は､すこしだけ期待して";
+window.im_cjs.t[6][2] = "います｡もしも､酔ってしまった";
+window.im_cjs.t[6][3] = "ら……ﾌﾟﾛﾃﾞｭｰｻｰさんの肩を､";
+window.im_cjs.t[6][4] = "貸してもらえるかも､なんて｡";
+
+window.im_cjs.t[7][1] = "半分は冗談です｡だけど､あと半";
+window.im_cjs.t[7][2] = "分は……本当かも｡桜は､私たち";
+window.im_cjs.t[7][3] = "の姿を隠してくれています｡";
+window.im_cjs.t[7][5] = "だから……いいかな､って♪";
