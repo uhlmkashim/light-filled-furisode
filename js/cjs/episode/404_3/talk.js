92c7355e2ce5b014549e56c9b9b87425f570288b
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-03-31 18:20:44 +0900

    Create talk.js

---
+++ js/cjs/episode/404_3/talk.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "……そっかそっか､JKになった";
+window.im_cjs.t[1][2] = "ばっかなんだ! これからが楽し";
+window.im_cjs.t[1][3] = "みだね★高校生活､楽しんで! ";
+window.im_cjs.t[1][4] = "じゃあね～♪";
+
+window.im_cjs.t[2][1] = "ふふっ! この写真はﾌｫﾙﾀﾞに入";
+window.im_cjs.t[2][2] = "れて……あ､ﾀﾞﾒﾀﾞﾒ､ﾌﾟﾛﾃﾞｭｰｻｰ! ";
+window.im_cjs.t[2][3] = "これは､ｱﾀｼだけの宝物! ﾌﾟﾛ";
+window.im_cjs.t[2][4] = "ﾃﾞｭｰｻｰには見せてあげない★";
+
+window.im_cjs.t[3][1] = "あの子たち､ｱﾀｼのﾌｧﾝなんだっ";
+window.im_cjs.t[3][2] = "て｡声かけてくれたからさ､記念";
+window.im_cjs.t[3][3] = "に､ｱﾀｼのｽﾏﾎでも一緒に写真";
+window.im_cjs.t[3][4] = "撮ったんだ～｡ﾍﾍｯ､ｲｲでしょ♪";
+
+window.im_cjs.t[4][1] = "帽子被ってても､すぐﾊﾞﾚちゃっ";
+window.im_cjs.t[4][2] = "た♪ｱﾀｼに会えて､元気もらった";
+window.im_cjs.t[4][3] = "って言ってくれてさー｡ｱﾀｼの方";
+window.im_cjs.t[4][4] = "こそ! ってｶﾝｼﾞだよ｡";
+
+window.im_cjs.t[5][1] = "……憧れなんだって｡美嘉ちゃ";
+window.im_cjs.t[5][2] = "んには､どんどん先に行ってほ";
+window.im_cjs.t[5][3] = "しいって言われちゃった｡さっ";
+window.im_cjs.t[5][4] = "すが､ｶﾘｽﾏでしょ? ﾍﾍｯ♪";
+
+window.im_cjs.t[6][1] = "ｱﾀｼと話すみんなの顔って､";
+window.im_cjs.t[6][2] = "最高にｷﾗｷﾗしてて……いつも､";
+window.im_cjs.t[6][3] = "すっごく元気出るんだ｡ﾎﾝﾄ……";
+window.im_cjs.t[6][4] = "ﾀﾞｻいこと､してらんないなっ!";
+
+window.im_cjs.t[7][1] = "うん､気合い入った! みんなの";
+window.im_cjs.t[7][2] = "ｶﾘｽﾏで､ﾄｯﾌﾟをひた走るｷﾞｬﾙ! ";
+window.im_cjs.t[7][3] = "それがｱﾀｼ｡";
+window.im_cjs.t[7][4] = "ﾌﾟﾛﾃﾞｭｰｻｰが信頼してる､";
+window.im_cjs.t[7][5] = "城ヶ崎美嘉! だよね★";
