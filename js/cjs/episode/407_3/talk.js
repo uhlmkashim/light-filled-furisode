86a775833c67c5ac21d1247bd2c7b85cccbc1ae4
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-04-30 16:24:20 +0900

    Create talk.js

---
+++ js/cjs/episode/407_3/talk.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "…………妙ですね｡地図が示す";
+window.im_cjs.t[1][2] = "先は､確かに､この辺りなのです";
+window.im_cjs.t[1][3] = "が……何故でしょう｡影も形も";
+window.im_cjs.t[1][4] = "……気配すら､ありません｡";
+
+window.im_cjs.t[2][1] = "路地裏にひっそりと佇む古書店";
+window.im_cjs.t[2][2] = "があると教えていただいたのに､";
+window.im_cjs.t[2][3] = "困りました｡……来た道は､あっ";
+window.im_cjs.t[2][4] = "ている筈なのですが｡";
+
+window.im_cjs.t[3][1] = "進めば進むほどに､深みへと嵌";
+window.im_cjs.t[3][2] = "っていく……そんな感覚です｡";
+window.im_cjs.t[3][3] = "ﾌﾟﾛﾃﾞｭｰｻｰさんを､手引きする";
+window.im_cjs.t[3][4] = "はずが､この有様では……｡";
+
+window.im_cjs.t[4][1] = "……励ましてくださるのですね｡";
+window.im_cjs.t[4][2] = "ありがとうございます｡ﾌﾟﾛﾃﾞｭｰ";
+window.im_cjs.t[4][3] = "ｻｰさんが､微笑んでくださると､";
+window.im_cjs.t[4][4] = "揺らぐ心も安らぎます｡";
+
+window.im_cjs.t[5][1] = "……ええ､そうですね｡下ばかり";
+window.im_cjs.t[5][2] = "を見ていては､見逃してしまい";
+window.im_cjs.t[5][3] = "ますね｡目の前に広がる……";
+window.im_cjs.t[5][4] = "こんなにも､美しい景色を｡";
+
+window.im_cjs.t[6][1] = "……私は､焦りすぎていたのか";
+window.im_cjs.t[6][2] = "もしれません｡貴方との大切な";
+window.im_cjs.t[6][3] = "旅の時間を､無駄にはするまい";
+window.im_cjs.t[6][4] = "と意固地になっていました｡";
+
+window.im_cjs.t[7][1] = "惑う時間も､貴方と過ごせば､";
+window.im_cjs.t[7][2] = "素敵な記憶へと変わります｡";
+window.im_cjs.t[7][3] = "ﾌﾟﾛﾃﾞｭｰｻｰさん､";
+window.im_cjs.t[7][4] = "もっとゆっくり……";
+window.im_cjs.t[7][5] = "思い出を作りましょうか｡";
