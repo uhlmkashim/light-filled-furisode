ff3f694b6eb764a625ef057321f9389b30fdf8fa
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-05-31 15:21:44 +0900

    Create talk.js

---
+++ js/cjs/episode/409_3/talk.js
@@ -0,0 +1,41 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "(……今のまゆは､眠り姫｡ただ";
+window.im_cjs.t[1][2] = "貴方の訪れを待って､永い時を､";
+window.im_cjs.t[1][3] = "夢の中で過ごし続ける､無力な";
+window.im_cjs.t[1][4] = "お姫様……)";
+
+window.im_cjs.t[2][1] = "(でも､本当は気づいているの｡";
+window.im_cjs.t[2][2] = "無為に眠り続けるだけのまゆは､";
+window.im_cjs.t[2][3] = "もうどこにもいない｡だって";
+window.im_cjs.t[2][4] = "……今は､知っているから)";
+
+window.im_cjs.t[3][1] = "(……あの人は､もうすぐここに";
+window.im_cjs.t[3][2] = "来てくれる｡そして､誓ってくれ";
+window.im_cjs.t[3][3] = "るの｡まゆと共に､ずっとずっと､";
+window.im_cjs.t[3][4] = "歩んでくれるって)";
+
+window.im_cjs.t[4][1] = "(ｱｲﾄﾞﾙとﾌﾟﾛﾃﾞｭｰｻｰ｡その未来は､";
+window.im_cjs.t[4][2] = "最初に思い描いたｷｽﾞﾅとは､違";
+window.im_cjs.t[4][3] = "うけれど……でも､それでいい";
+window.im_cjs.t[4][4] = "……ううん｡それがいいの)";
+
+window.im_cjs.t[5][1] = "(一番近くにいられるだけじゃ";
+window.im_cjs.t[5][2] = "ない……｡貴方が語る未来を､貴";
+window.im_cjs.t[5][3] = "方が望む未来を､叶えていける｡";
+window.im_cjs.t[5][4] = "まゆが､叶えてあげられる)";
+
+window.im_cjs.t[6][1] = "……うふふ､やっぱり来てくれ";
+window.im_cjs.t[6][2] = "た､ﾌﾟﾛﾃﾞｭｰｻｰさん｡では……";
+window.im_cjs.t[6][3] = "どうか､まゆに誓ってください｡";
+window.im_cjs.t[6][4] = "貴方の､その言葉で｡";
+
+window.im_cjs.t[7][1] = "まゆは､誓います｡この出会いは､";
+window.im_cjs.t[7][2] = "運命だから……｡まゆは､ずっと､";
+window.im_cjs.t[7][3] = "ずっと､ずぅーっと……";
+window.im_cjs.t[7][4] = "ﾌﾟﾛﾃﾞｭｰｻｰさんだけのｱｲﾄﾞﾙです｡";
