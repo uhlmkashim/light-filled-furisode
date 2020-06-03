2d7c5d45d24fa03ca0ed6ee53762ca2e7326a766
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-03 21:57:44 +0900

    Create talk_k.js

---
+++ js/cjs/episode/407_3/talk_k.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "…………묘하네요. 지도가 나타내는 곳은,";
+window.im_cjs.t[1][2] = "확실히, 이 근처입니다만";
+window.im_cjs.t[1][3] = "… 어째서일까요. 그림자도 형태도";
+window.im_cjs.t[1][4] = "…… 기척조차, 없습니다.";
+
+window.im_cjs.t[2][1] = "골목 뒤쪽에 조용히 자리잡은 고서점이";
+window.im_cjs.t[2][2] = "있다고 가르쳐 주셨는데,";
+window.im_cjs.t[2][3] = "곤란합니다. ……온 길은,";
+window.im_cjs.t[2][4] = "맞을 것입니다만.";
+
+window.im_cjs.t[3][1] = "나아가면 나아갈수록, 깊게 빠져들어";
+window.im_cjs.t[3][2] = "가는…… 그런 감각입니다.";
+window.im_cjs.t[3][3] = "프로듀서 씨를, 안내하는 것";
+window.im_cjs.t[3][4] = "이, 이 상태라면…….";
+
+window.im_cjs.t[4][1] = "……격려해 주시는 거군요.";
+window.im_cjs.t[4][2] = "감사합니다. 프로듀서 씨";
+window.im_cjs.t[4][3] = "가, 미소 지어 주시면,";
+window.im_cjs.t[4][4] = "요동치던 마음도 편안해집니다.";
+
+window.im_cjs.t[5][1] = "……네, 그렇네요. 아래만";
+window.im_cjs.t[5][2] = "보고 있으면, 놓쳐버리는";
+window.im_cjs.t[5][3] = "군요. 눈 앞에 펼쳐진……";
+window.im_cjs.t[5][4] = "이렇게도, 아름다운 경치를.";
+
+window.im_cjs.t[6][1] = "……저는, 초조해 하고 있었는지도";
+window.im_cjs.t[6][2] = "모릅니다. 당신과의 소중한";
+window.im_cjs.t[6][3] = "여행 시간을, 헛되이 하지 않겠다고";
+window.im_cjs.t[6][4] = "고집을 부리고 있었습니다.";
+
+window.im_cjs.t[7][1] = "갈팡거리는 시간도, 당신과 보낸다면,";
+window.im_cjs.t[7][2] = "멋진 기억으로 바뀝니다.";
+window.im_cjs.t[7][3] = "프로듀서 씨,";
+window.im_cjs.t[7][4] = "좀 더 느긋이……";
+window.im_cjs.t[7][5] = "추억을 만들까요.";
