904c307912afd1500140ca608c243e327022a11e
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-06 18:46:55 +0900

    Create talk_k.js

---
+++ js/cjs/episode/409_3/talk_k.js
@@ -0,0 +1,41 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "(지금의 마유는, 잠자는 공주. 단지";
+window.im_cjs.t[1][2] = "당신이 찾아오기를 기다리며, 오랜 시간을,";
+window.im_cjs.t[1][3] = "꿈 속에서 보내는, 무력한";
+window.im_cjs.t[1][4] = "공주님……)";
+
+window.im_cjs.t[2][1] = "(하지만, 사실은 눈치채고 있어.";
+window.im_cjs.t[2][2] = "부질없이 잠만 자는 마유는,";
+window.im_cjs.t[2][3] = "이제 어디에도 없어. 왜냐하면";
+window.im_cjs.t[2][4] = "……지금은, 알고 있으니까)";
+
+window.im_cjs.t[3][1] = "(……그 사람은, 곧 여기에";
+window.im_cjs.t[3][2] = "와 줄거야. 그리고, 맹세해 줄거";
+window.im_cjs.t[3][3] = "야. 마유와 함께, 계속 계속,";
+window.im_cjs.t[3][4] = "걸어줄 거라고)";
+
+window.im_cjs.t[4][1] = "(아이돌과 프로듀서. 그 미래는,";
+window.im_cjs.t[4][2] = "최초에 마음에 그린 인연과는, 다르";
+window.im_cjs.t[4][3] = "지만…… 그래도, 그걸로 좋아";
+window.im_cjs.t[4][4] = "……아니. 그게 좋아)";
+
+window.im_cjs.t[5][1] = "(가장 가까이 있을 뿐만이 아";
+window.im_cjs.t[5][2] = "니라……. 당신이 말하는 미래를, 당";
+window.im_cjs.t[5][3] = "신이 바라는 미래를, 이룰 수 있어.";
+window.im_cjs.t[5][4] = "마유가, 이뤄줄 수 있어)";
+
+window.im_cjs.t[6][1] = "……우후후, 역시 와주셨네";
+window.im_cjs.t[6][2] = "요, 프로듀서 씨. 그럼……";
+window.im_cjs.t[6][3] = "부디, 마유에게 맹세해 주세요.";
+window.im_cjs.t[6][4] = "당신의, 그 말로.";
+
+window.im_cjs.t[7][1] = "마유는, 맹세해요. 이 만남은,";
+window.im_cjs.t[7][2] = "운명이니까……. 마유는, 계속,";
+window.im_cjs.t[7][3] = "계속, 계~속……";
+window.im_cjs.t[7][4] = "프로듀서 씨만의 아이돌입니다.";
