d693d5531e26f5edf07a24edcebe9dc797481452
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-08 03:25:04 +0900

    Create talk_k.js

---
+++ js/cjs/episode/394_4/talk_k.js
@@ -0,0 +1,41 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];﻿
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "후우……상당히 올라왔네";
+window.im_cjs.t[1][2] = "……. 괘, 괜찮아, 프로듀서";
+window.im_cjs.t[1][3] = "……조금 걷기 힘들지만……";
+window.im_cjs.t[1][4] = "어, 어떻게든 노력하고 있어…….";
+
+window.im_cjs.t[2][1] = "사무소 모두가, 애써서 입혀";
+window.im_cjs.t[2][2] = "줬어……옷매무새를, 흐트리고";
+window.im_cjs.t[2][3] = "싶지 않아. 기모노는……고, 공주님";
+window.im_cjs.t[2][4] = "같아서, 부끄럽지만…….";
+
+window.im_cjs.t[3][1] = "어, 어, 어어 어울려!? ……";
+window.im_cjs.t[3][2] = "…………아, 미안해. 예상 외의";
+window.im_cjs.t[3][3] = "말이 들려와서, 얼어붙어 버렸어.";
+window.im_cjs.t[3][4] = "그……고, 고마워.";
+
+window.im_cjs.t[4][1] = "하츠모데는, 신님에게 인사하는 장소……";
+window.im_cjs.t[4][2] = "그러니까 예쁜 몸인 쪽이, 분명";
+window.im_cjs.t[4][3] = "효험있어. 후힛……무엇을 원하";
+window.im_cjs.t[4][4] = "는지도, 이미 정했어."
+
+window.im_cjs.t[5][1] = "1번째는, 가족과 친구의 건강.";
+window.im_cjs.t[5][2] = "응, 소중해. 2번째는, 사무소의 번";
+window.im_cjs.t[5][3] = "창. 이것도, 응. 그리고, 3번째.";
+window.im_cjs.t[5][4] = "이것이 가장 소중한 녀석…….";
+
+window.im_cjs.t[6][1] = "히얏!? ……미, 미안. 단차";
+window.im_cjs.t[6][2] = "에 발이 걸렸어. 괜찮아. 하지만……";
+window.im_cjs.t[6][3] = "괘, 괜찮다면, 이대로 팔을";
+window.im_cjs.t[6][4] = "빌려도, 될까……?";
+
+window.im_cjs.t[7][1] = "이건, 3번째 소원이기도, 해.";
+window.im_cjs.t[7][2] = "프로듀서와 이 앞으로도, 한걸음 한걸음";
+window.im_cjs.t[7][3] = "……손을 맞잡고, 나아가고 싶어.";
+window.im_cjs.t[7][4] = "후히……올해도, 잘 부탁해…….";
