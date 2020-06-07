fd73ccac0b3bd9b36d46309bf287c3740c1c7e55
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-08 02:59:31 +0900

    Create talk.js

---
+++ js/cjs/episode/383_2/talk.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];﻿
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "うん……いい日♪こうしてﾌﾟﾛ";
+window.im_cjs.t[1][2] = "ﾃﾞｭｰｻｰさんと肩を並べて､ゆっ";
+window.im_cjs.t[1][3] = "くりお散歩できるんだもの｡";
+window.im_cjs.t[1][4] = "まゆ､とっても幸せ……♪";
+
+window.im_cjs.t[2][1] = "街路樹も､赤く染まって､綺麗";
+window.im_cjs.t[2][2] = "……♪きっと､秋は短いから";
+window.im_cjs.t[2][3] = "……大切な人に見てもらえるよ";
+window.im_cjs.t[2][4] = "う､懸命に色づいているのかも｡";
+
+window.im_cjs.t[3][1] = "ねぇ､ﾌﾟﾛﾃﾞｭｰｻｰさん……お着物";
+window.im_cjs.t[3][2] = "姿のまゆ､気に入ってくれまし";
+window.im_cjs.t[3][3] = "た? いつもより､大人なまゆ";
+window.im_cjs.t[3][4] = "……意識してみました♪";
+
+window.im_cjs.t[4][1] = "うふふっ､よかった……♪ﾌﾟﾛ";
+window.im_cjs.t[4][2] = "ﾃﾞｭｰｻｰさんが望むなら､どんな";
+window.im_cjs.t[4][3] = "まゆにだってなれるけど……";
+window.im_cjs.t[4][4] = "今日は､強く思ったんです｡";
+
+window.im_cjs.t[5][1] = "なんでかな……｡いつもは着な";
+window.im_cjs.t[5][2] = "い､お着物だから? 髪を結って";
+window.im_cjs.t[5][3] = "いるから? それとも……ちょっ";
+window.im_cjs.t[5][4] = "ぴり切ない秋､だから?";
+
+window.im_cjs.t[6][1] = "理由はわかりません｡でも……";
+window.im_cjs.t[6][2] = "どれも､些細なことですよね｡足";
+window.im_cjs.t[6][3] = "早に過ぎ去る季節を､貴方と過";
+window.im_cjs.t[6][4] = "ごせる喜びと比べたら♪";
+
+window.im_cjs.t[7][1] = "だから､ﾌﾟﾛﾃﾞｭｰｻｰさん｡もう少";
+window.im_cjs.t[7][2] = "しだけ……まゆと､のんびり､歩";
+window.im_cjs.t[7][3] = "きませんか? ";
+window.im_cjs.t[7][4] = "ふたりの時間が､";
+window.im_cjs.t[7][5] = "ずぅーっと続くように……♪";
