658ccdc85ef20ff51064f572c1d08a78384feefc
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-08 20:58:21 +0900

    Create 399_2_k.js

---
+++ js/cjs/episode/399_2/399_2_k.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "유채꽃이, 너무 예쁘네요.";
+window.im_cjs.t[1][2] = "이 부근은, 이제 완전히 봄이";
+window.im_cjs.t[1][3] = "찾아온 것 같아……. 상당히,";
+window.im_cjs.t[1][4] = "남쪽까지 왔네요.";
+
+window.im_cjs.t[2][1] = "비행기로 잠깐 날아와도 괜찮지";
+window.im_cjs.t[2][2] = "만…… 덜컹덜컹하고 흔들리며,";
+window.im_cjs.t[2][3] = "흐르는 경치를 느긋이 즐길 수 있는";
+window.im_cjs.t[2][4] = "것은, 기차 여행뿐이네요.";
+
+window.im_cjs.t[3][1] = "거기에, 지방 명물 도시락과, 차가";
+window.im_cjs.t[3][2] = "운 맥주. 푸른 하늘과, 만발한 유채";
+window.im_cjs.t[3][3] = "꽃. 그리고, 프로듀서와의 즐거운";
+window.im_cjs.t[3][4] = "대화도. 정말로 사치…….";
+
+window.im_cjs.t[4][1] = "그런 사치스러운 시간을 태우고,";
+window.im_cjs.t[4][2] = "열차는 우리를 어디에 데려다";
+window.im_cjs.t[4][3] = "주는 걸까요…… 라니.";
+window.im_cjs.t[4][4] = "목적지는, 알고 있어요.";
+
+window.im_cjs.t[5][1] = "온천, 정말로 기대돼요.";
+window.im_cjs.t[5][2] = "프로듀서가 추천해준 그대로,";
+window.im_cjs.t[5][3] = "따뜻한 물에 담겨서 릴렉스하는";
+window.im_cjs.t[5][4] = "거, 저, 정말 좋아하니까요.";
+
+window.im_cjs.t[6][1] = "그래도, 도착까지는 아직 한참 ";
+window.im_cjs.t[6][2] = "걸리고…… 우선 눈 앞에 있는 도시";
+window.im_cjs.t[6][3] = "락과 술로, 마음의 피로를 풀까요.";
+window.im_cjs.t[6][4] = "자, 또 한 잔.";
+
+window.im_cjs.t[7][1] = "아니면, 디저트를 원하시나요?";
+window.im_cjs.t[7][2] = "그렇다면, 귤을 부디.";
+window.im_cjs.t[7][3] = "오늘은 저도……";
+window.im_cjs.t[7][4] = "프로듀서와,";
+window.im_cjs.t[7][5] = "느긋이 즐길게요♪";
