20e0764b5fb699b01d4ffac4f968a7618cf72040
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-21 02:27:49 +0900

    Update talk.js

--- js/cjs/episode/404_4/talk.js
+++ js/cjs/episode/404_4/talk.js
@@ -5,37 +5,37 @@ for (var i = 0, len = 10; i < len; i++) {
 }
 
 //______________________"１２３４５６７８９０１２３４5"
-window.im_cjs.t[1][1] = "저……프로듀서 씨? 그렇게";
-window.im_cjs.t[1][2] = "바라봐주지 않으셔도,";
-window.im_cjs.t[1][3] = "괜찮아요. 곁에 있고……";
-window.im_cjs.t[1][4] = "미아가 되지는 않을테니까요.";
+window.im_cjs.t[1][1] = "あの……ﾌﾟﾛﾃﾞｭｰｻｰさん? そん";
+window.im_cjs.t[1][2] = "なに見つめていただかなくても､";
+window.im_cjs.t[1][3] = "大丈夫です｡そばにいますし……";
+window.im_cjs.t[1][4] = "迷子にはなりませんから｡";
 
-window.im_cjs.t[2][1] = "그렇지 않으면…… 벚꽃의 아름다움에,";
-window.im_cjs.t[2][2] = "사로잡혀 버리셨나요? 그렇다면";
-window.im_cjs.t[2][3] = "……저도, 같네요. 아름답고,";
-window.im_cjs.t[2][4] = "멋지게 피어있네요.";
+window.im_cjs.t[2][1] = "それとも……桜の美しさに､捕";
+window.im_cjs.t[2][2] = "らわれてしまいましたか? それ";
+window.im_cjs.t[2][3] = "なら……私も､同じです｡美しく､";
+window.im_cjs.t[2][4] = "見事に咲いていますね｡";
 
-window.im_cjs.t[3][1] = "여기 올 때까지…… 조금, 걱정";
-window.im_cjs.t[3][2] = "하고 있었어요. 사람들이 많이";
-window.im_cjs.t[3][3] = "모이는 장소니까, 조용히";
-window.im_cjs.t[3][4] = "즐길 수 있을지…….";
+window.im_cjs.t[3][1] = "ここに来るまで……少し､心配";
+window.im_cjs.t[3][2] = "していたんです｡人がたくさん";
+window.im_cjs.t[3][3] = "集まる場所だから､ひっそりと";
+window.im_cjs.t[3][4] = "楽しめるか……｡";
 
-window.im_cjs.t[4][1] = "하지만……기우였던 것 같네요.";
-window.im_cjs.t[4][2] = "모두, 벚꽃에 넋을 잃고 보느라";
-window.im_cjs.t[4][3] = "…… 분명 아무도, 우리를 눈치";
-window.im_cjs.t[4][4] = "채지 못했으니까요.";
+window.im_cjs.t[4][1] = "けど……杞憂だったみたいです";
+window.im_cjs.t[4][2] = "ね｡みなさん､桜に見惚れていて";
+window.im_cjs.t[4][3] = "……きっと誰も､私たちには気";
+window.im_cjs.t[4][4] = "づいていませんから｡";
 
-window.im_cjs.t[5][1] = "그러니까, 오늘은…… 조금";
-window.im_cjs.t[5][2] = "술도 즐겨볼까 해서.";
-window.im_cjs.t[5][3] = "후훗, 보세요. 딸기 샴페인,";
-window.im_cjs.t[5][4] = "귀엽죠……♪";
+window.im_cjs.t[5][1] = "だから､今日は……ちょっぴり";
+window.im_cjs.t[5][2] = "お酒も楽しんじゃおうかなって｡";
+window.im_cjs.t[5][3] = "ふふっ､見てください｡苺のｼｬﾝﾊﾟ";
+window.im_cjs.t[5][4] = "ﾝ､可愛いですよね……♪";
 
-window.im_cjs.t[6][1] = "……사실, 조금 기대하고 있어요.";
-window.im_cjs.t[6][2] = "만약, 취해 버린다면……";
-window.im_cjs.t[6][3] = "프로듀서 씨의 어깨를,";
-window.im_cjs.t[6][4] = "빌릴 수 있을지도, 라던지.";
+window.im_cjs.t[6][1] = "……実は､すこしだけ期待して";
+window.im_cjs.t[6][2] = "います｡もしも､酔ってしまった";
+window.im_cjs.t[6][3] = "ら……ﾌﾟﾛﾃﾞｭｰｻｰさんの肩を､";
+window.im_cjs.t[6][4] = "貸してもらえるかも､なんて｡";
 
-window.im_cjs.t[7][1] = "반은 농담이에요. 하지만, 나머지";
-window.im_cjs.t[7][2] = "반은…… 정말일지도. 벚꽃이, 우리의";
-window.im_cjs.t[7][3] = "모습을 숨겨주고 있어요.";
-window.im_cjs.t[7][5] = "그러니까…… 괜찮을지도, 요♪";
+window.im_cjs.t[7][1] = "半分は冗談です｡だけど､あと半";
+window.im_cjs.t[7][2] = "分は……本当かも｡桜は､私たち";
+window.im_cjs.t[7][3] = "の姿を隠してくれています｡";
+window.im_cjs.t[7][5] = "だから……いいかな､って♪";
