2a4f48a496b4d79cf9a89a539fbf86695faf9820
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-20 19:44:11 +0900

    Create talk.js

---
+++ js/cjs/episode/391_2/talk.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];﻿
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "……つかまえた……｡ふふ……";
+window.im_cjs.t[1][2] = "驚いた……? 私……ﾌﾟﾛﾃﾞｭｰｻｰ";
+window.im_cjs.t[1][3] = "なら……すぐ見つけられる……｡";
+window.im_cjs.t[1][4] = "いつも……そう……｡";
+
+window.im_cjs.t[2][1] = "大丈夫……私も……来たばかり";
+window.im_cjs.t[2][2] = "……｡ﾌﾟﾛﾃﾞｭｰｻｰ……早く来ると";
+window.im_cjs.t[2][3] = "思ってた……から……｡待って";
+window.im_cjs.t[2][4] = "ない……本当……｡";
+
+window.im_cjs.t[3][1] = "見たいお店……調べてきた……｡";
+window.im_cjs.t[3][2] = "楽しみにしてた……から……｡";
+window.im_cjs.t[3][3] = "ﾌﾟﾛﾃﾞｭｰｻｰと……きっと……";
+window.im_cjs.t[3][4] = "素敵なもの……見つかる……｡";
+
+window.im_cjs.t[4][1] = "気に入ったもの……あったら…";
+window.im_cjs.t[4][2] = "…教えて……? ﾌﾟﾛﾃﾞｭｰｻｰの……";
+window.im_cjs.t[4][3] = "好きなもの……教えて……｡";
+window.im_cjs.t[4][4] = "だって……ﾌﾟﾚｾﾞﾝﾄは……｡";
+
+window.im_cjs.t[5][1] = "ﾌﾟﾚｾﾞﾝﾄは……ﾌﾟﾛﾃﾞｭｰｻｰに……";
+window.im_cjs.t[5][2] = "あげるの……｡私の……大切な";
+window.im_cjs.t[5][3] = "人……｡ありがとうを……";
+window.im_cjs.t[5][4] = "たくさん……伝えたい人……｡";
+
+window.im_cjs.t[6][1] = "それじゃ……だめ……? ﾌﾟﾛﾃﾞｭｰ";
+window.im_cjs.t[6][2] = "ｻｰも……私に……ありがとう";
+window.im_cjs.t[6][3] = "のﾌﾟﾚｾﾞﾝﾄ……? そっか……";
+window.im_cjs.t[6][4] = "同じ……気持ちだった……｡";
+
+window.im_cjs.t[7][1] = "じゃあ……わがまま……聞いて";
+window.im_cjs.t[7][2] = "……｡手……ぎゅって繋いで……";
+window.im_cjs.t[7][3] = "離さないで……? ";
+window.im_cjs.t[7][4] = "一緒に並んで……行こう……";
+window.im_cjs.t[7][5] = "ね……｡";
