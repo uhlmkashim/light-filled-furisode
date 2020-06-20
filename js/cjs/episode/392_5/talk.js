13327582b42186fbe78ada4338863aa1eff064cb
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-20 21:55:26 +0900

    Create talk.js

---
+++ js/cjs/episode/392_5/talk.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];﻿
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "そなたー､そなたー｡ご覧なさい";
+window.im_cjs.t[1][2] = "ませー｡なんとも美しい眺めで";
+window.im_cjs.t[1][3] = "してー｡わたくしも､思わず息を";
+window.im_cjs.t[1][4] = "のんでしまったほどー｡";
+
+window.im_cjs.t[2][1] = "目覚めたばかりの空気は清らか";
+window.im_cjs.t[2][2] = "でー｡雪の花は暁光に輝き､一面";
+window.im_cjs.t[2][3] = "の風景は白くー｡別世界に迷い";
+window.im_cjs.t[2][4] = "込んだようでしてー｡";
+
+window.im_cjs.t[3][1] = "わたくしを呼ぶ声がしたゆえ､";
+window.im_cjs.t[3][2] = "早起きしたのですよー｡きっと";
+window.im_cjs.t[3][3] = "この景色が､おいでーおいでー､";
+window.im_cjs.t[3][4] = "と呼んでいたのでしょー｡";
+
+window.im_cjs.t[4][1] = "ですがー､美しい白銀も､いずれ";
+window.im_cjs.t[4][2] = "溶けて消えてしまうさだめー｡";
+window.im_cjs.t[4][3] = "そう思うと､寂しき風が心に吹";
+window.im_cjs.t[4][4] = "くのでしてー……｡";
+
+window.im_cjs.t[5][1] = "ふふふー､心配なさらずともー｡";
+window.im_cjs.t[5][2] = "そなたと眺めた記憶は消えませ";
+window.im_cjs.t[5][3] = "ぬー｡わたくしの奥で､永久にあ";
+window.im_cjs.t[5][4] = "り続けましょー｡";
+
+window.im_cjs.t[6][1] = "さてー……みなが起きるまで､";
+window.im_cjs.t[6][2] = "まだ充分に時間がありましてー｡";
+window.im_cjs.t[6][3] = "せっかくの銀世界､しばし先ま";
+window.im_cjs.t[6][4] = "で足を延ばしましょー｡";
+
+window.im_cjs.t[7][1] = "足元もゆるりとー｡一歩一歩､";
+window.im_cjs.t[7][2] = "しっかり踏みかためてー｡心に";
+window.im_cjs.t[7][3] = "積もる､";
+window.im_cjs.t[7][4] = "そなたとの､あたたかく";
+window.im_cjs.t[7][5] = "優しい記憶とともにー｡";
