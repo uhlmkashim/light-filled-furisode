5e2187d1b8b6384fc5caed29672102985f2824ba
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-21 02:27:23 +0900

    Create talk_k.js

---
+++ js/cjs/episode/404_4/talk_k.js
@@ -0,0 +1,41 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "저……프로듀서 씨? 그렇게";
+window.im_cjs.t[1][2] = "바라봐주지 않으셔도,";
+window.im_cjs.t[1][3] = "괜찮아요. 곁에 있고……";
+window.im_cjs.t[1][4] = "미아가 되지는 않을테니까요.";
+
+window.im_cjs.t[2][1] = "그렇지 않으면…… 벚꽃의 아름다움에,";
+window.im_cjs.t[2][2] = "사로잡혀 버리셨나요? 그렇다면";
+window.im_cjs.t[2][3] = "……저도, 같네요. 아름답고,";
+window.im_cjs.t[2][4] = "멋지게 피어있네요.";
+
+window.im_cjs.t[3][1] = "여기 올 때까지…… 조금, 걱정";
+window.im_cjs.t[3][2] = "하고 있었어요. 사람들이 많이";
+window.im_cjs.t[3][3] = "모이는 장소니까, 조용히";
+window.im_cjs.t[3][4] = "즐길 수 있을지…….";
+
+window.im_cjs.t[4][1] = "하지만……기우였던 것 같네요.";
+window.im_cjs.t[4][2] = "모두, 벚꽃에 넋을 잃고 보느라";
+window.im_cjs.t[4][3] = "…… 분명 아무도, 우리를 눈치";
+window.im_cjs.t[4][4] = "채지 못했으니까요.";
+
+window.im_cjs.t[5][1] = "그러니까, 오늘은…… 조금";
+window.im_cjs.t[5][2] = "술도 즐겨볼까 해서.";
+window.im_cjs.t[5][3] = "후훗, 보세요. 딸기 샴페인,";
+window.im_cjs.t[5][4] = "귀엽죠……♪";
+
+window.im_cjs.t[6][1] = "……사실, 조금 기대하고 있어요.";
+window.im_cjs.t[6][2] = "만약, 취해 버린다면……";
+window.im_cjs.t[6][3] = "프로듀서 씨의 어깨를,";
+window.im_cjs.t[6][4] = "빌릴 수 있을지도, 라던지.";
+
+window.im_cjs.t[7][1] = "반은 농담이에요. 하지만, 나머지";
+window.im_cjs.t[7][2] = "반은…… 정말일지도. 벚꽃이, 우리의";
+window.im_cjs.t[7][3] = "모습을 숨겨주고 있어요.";
+window.im_cjs.t[7][5] = "그러니까…… 괜찮을지도, 요♪";
