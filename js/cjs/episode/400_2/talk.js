bb6ff304d0d19f7d239202d6fa00a80baa7b5973
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-21 03:42:19 +0900

    Create talk.js

---
+++ js/cjs/episode/400_2/talk.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "ﾌﾟﾛﾃﾞｭｰｻｰさんも､おねむですか?";
+window.im_cjs.t[1][2] = "……なんて｡私､まだ保育士さん";
+window.im_cjs.t[1][3] = "気分が抜けないんです｡すっご";
+window.im_cjs.t[1][4] = "く貴重な体験でしたから｡";
+
+window.im_cjs.t[2][1] = "だから……お疲れのﾌﾟﾛﾃﾞｭｰｻｰ";
+window.im_cjs.t[2][2] = "さん､響子先生と一緒にお昼寝";
+window.im_cjs.t[2][3] = "しませんか? ﾌﾞﾗﾝｹｯﾄをかけて､";
+window.im_cjs.t[2][4] = "子守唄を歌ってあげます♪";
+
+window.im_cjs.t[3][1] = "髪も､ほどいちゃいましたから";
+window.im_cjs.t[3][2] = "ね｡ｸｾになっちゃうといけない";
+window.im_cjs.t[3][3] = "ですし……ﾌﾟﾛﾃﾞｭｰｻｰさんとの";
+window.im_cjs.t[3][4] = "添い寝もしづらいですから｡";
+
+window.im_cjs.t[4][1] = "ふふっ､新鮮ですか? ｻｲﾄﾞﾃｰﾙは､";
+window.im_cjs.t[4][2] = "私のﾁｬｰﾑﾎﾟｲﾝﾄだから｡家事がし";
+window.im_cjs.t[4][3] = "やすくて､可愛くて……ｱｲﾄﾞﾙら";
+window.im_cjs.t[4][4] = "しくて､好きなんです｡";
+
+window.im_cjs.t[5][1] = "でも､いつもの私は少しお休み";
+window.im_cjs.t[5][2] = "です｡今､ﾌﾟﾛﾃﾞｭｰｻｰさんの隣に";
+window.im_cjs.t[5][3] = "いるのは､一緒にお昼寝がした";
+window.im_cjs.t[5][4] = "い､やさしい響子先生ですよ♪";
+
+window.im_cjs.t[6][1] = "それに､先生は知ってるんです｡";
+window.im_cjs.t[6][2] = "ﾌﾟﾛﾃﾞｭｰｻｰさんなら､わかってく";
+window.im_cjs.t[6][3] = "れること……夢から醒めたとき､";
+window.im_cjs.t[6][4] = "隣にいるのは､私だって｡";
+
+window.im_cjs.t[7][1] = "……ﾌﾟﾛﾃﾞｭｰｻｰさん､寝ちゃいま";
+window.im_cjs.t[7][2] = "した? じゃあ……今日も､お仕事";
+window.im_cjs.t[7][3] = "よく頑張りました♪";
+window.im_cjs.t[7][4] = "なでなで……";
+window.im_cjs.t[7][5] = "おやすみなさい♪";
