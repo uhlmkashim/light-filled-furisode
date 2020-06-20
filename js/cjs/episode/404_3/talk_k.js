c8239498d82f67c76420307d3327239540bf90f6
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-21 01:52:02 +0900

    Create talk_k.js

---
+++ js/cjs/episode/404_3/talk_k.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "……그래그래, JK가 된지";
+window.im_cjs.t[1][2] = "얼마 안됐구나! 이제부터가 즐거움";
+window.im_cjs.t[1][3] = "이네★ 고교생활, 즐겁게 보내!";
+window.im_cjs.t[1][4] = "그럼 이만~♪";
+
+window.im_cjs.t[2][1] = "후훗! 이 사진은 폴더에 넣어서";
+window.im_cjs.t[2][2] = "……아, 안돼안돼, 프로듀서!";
+window.im_cjs.t[2][3] = "이건, 나만의 보물!";
+window.im_cjs.t[2][4] = "프로듀서에겐 보여주지 않아★";
+
+window.im_cjs.t[3][1] = "저 애들, 내 팬이래.";
+window.im_cjs.t[3][2] = "말을 걸어줬으니까, 기념으로,";
+window.im_cjs.t[3][3] = "내 스마트폰으로도 같이 사진";
+window.im_cjs.t[3][4] = "찍었어~. 헤헷, 좋잖아♪";
+
+window.im_cjs.t[4][1] = "모자를 쓰고 있어도, 바로 들켰어♪";
+window.im_cjs.t[4][2] = "나를 만나서, 기운을 얻었다고";
+window.im_cjs.t[4][3] = "말해줘서 말야~. 내 쪽이야말로!";
+window.im_cjs.t[4][4] = "라는 느낌이야.";
+
+window.im_cjs.t[5][1] = "……동경한다고. 미카 쨩은,";
+window.im_cjs.t[5][2] = "척척 앞서 나가주길 바란다는";
+window.im_cjs.t[5][3] = "말을 들었어. 역시,";
+window.im_cjs.t[5][4] = "카리스마지? 헤헷♪";
+
+window.im_cjs.t[6][1] = "나와 이야기하는 모두의 얼굴은,";
+window.im_cjs.t[6][2] = "최고로 반짝반짝해서……언제나,";
+window.im_cjs.t[6][3] = "굉장히 힘이 나네. 정말……";
+window.im_cjs.t[6][4] = "멋없는 일, 못하겠네!";
+
+window.im_cjs.t[7][1] = "응, 기합이 들었어! 모두의";
+window.im_cjs.t[7][2] = "카리스마로, 톱을 향해 달리는 갸루!";
+window.im_cjs.t[7][3] = "그것이 나.";
+window.im_cjs.t[7][4] = "프로듀서가 신뢰하는,";
+window.im_cjs.t[7][5] = "죠가사키 미카! 그렇지★";
