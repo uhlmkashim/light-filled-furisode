3ac24a367258451d0eb9789ea7b4d8548b028086
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-21 01:29:48 +0900

    Create talk_k.js

---
+++ js/cjs/episode/403_2/talk_k.js
@@ -0,0 +1,43 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "띵똥~";
+
+window.im_cjs.t[2][1] = "네네, 열게~♪기다려줘~";
+window.im_cjs.t[2][2] = "……, 뭐야 너인가~.";
+window.im_cjs.t[2][3] = "마침 지금, 좋은 때네.";
+window.im_cjs.t[2][4] = "Shhh,be quiet.Okay?";
+
+window.im_cjs.t[3][1] = "응, 괜찮아♪그래서, 너는 뭘";
+window.im_cjs.t[3][2] = "하러 왔을까나~? 여자애 동산에,";
+window.im_cjs.t[3][3] = "설마 아무것도 준비 안하고 왔다면";
+window.im_cjs.t[3][4] = "도리가 아니겠지?";
+
+window.im_cjs.t[4][1] = "거드름피우지 말고 내밀어 봐.";
+window.im_cjs.t[4][2] = "너의 뒤에, 숨기고 있지?";
+window.im_cjs.t[4][3] = "탐욕스러운 여자애들이 원하는,";
+window.im_cjs.t[4][4] = "정말 좋아하는 그것 말야.";
+
+window.im_cjs.t[5][1] = "숨겨봤자 알 수 있어~. 그 냄새는,";
+window.im_cjs.t[5][2] = "상영회를 좀 더 근사한 무언가로";
+window.im_cjs.t[5][3] = "바꿔줄 수 있는 것…… 예를 들면,";
+window.im_cjs.t[5][4] = "치즈와 토마토 냄새.";
+
+window.im_cjs.t[6][1] = "즉, 너의 등뒤에 숨어있는 건";
+window.im_cjs.t[6][2] = "…… 주문한 초특대･딜럭스";
+window.im_cjs.t[6][3] = "스페셜리티 피자라고 봐! 오,";
+window.im_cjs.t[6][4] = "정답! 응하~, 맛있어~♪";
+
+window.im_cjs.t[7][1] = "사다줘서, 고마워!";
+window.im_cjs.t[7][2] = "……라니, 안돼. 큰 소리를";
+window.im_cjs.t[7][3] = "내버렸어. 흥분해버려서,";
+window.im_cjs.t[7][4] = "그만♪조용히네, 조용히.";
+
+window.im_cjs.t[8][1] = "그래도~, 역시 영화에는 정크";
+window.im_cjs.t[8][2] = "푸드야. ……응, 너의 참가를";
+window.im_cjs.t[8][3] = "허가하지! 자, 들어와.";
+window.im_cjs.t[8][4] = "비밀의 시어터에 잘 왔어♪";
