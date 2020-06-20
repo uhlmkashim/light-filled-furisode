c531943c9763a05b90765134e7aafa92f2356ed7
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-20 20:18:48 +0900

    Create talk_k.js

---
+++ js/cjs/episode/391_2/talk_k.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];﻿
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "……잡았다……. 후후……";
+window.im_cjs.t[1][2] = "놀랐어……? 나…… 프로듀서";
+window.im_cjs.t[1][3] = "라면……금방 찾을 수 있어…….";
+window.im_cjs.t[1][4] = "언제나…… 그래…….";
+
+window.im_cjs.t[2][1] = "괜찮아…… 나도…… 방금 왔어";
+window.im_cjs.t[2][2] = "……. 프로듀서…… 빨리 올거라고";
+window.im_cjs.t[2][3] = "생각…… 했으니까……. 기다리지";
+window.im_cjs.t[2][4] = "않았어…… 정말로…….";
+
+window.im_cjs.t[3][1] = "보고 싶은 가게…… 조사해 왔어…….";
+window.im_cjs.t[3][2] = "기대하고 있었…… 으니까…….";
+window.im_cjs.t[3][3] = "프로듀서와…… 분명……";
+window.im_cjs.t[3][4] = "멋진 것…… 찾을 수 있어…….";
+
+window.im_cjs.t[4][1] = "마음에 드는 것…… 있다면…";
+window.im_cjs.t[4][2] = "…알려줘……? 프로듀서의……";
+window.im_cjs.t[4][3] = "좋아하는 것…… 가르쳐줘…….";
+window.im_cjs.t[4][4] = "왜냐하면…… 선물은…….";
+
+window.im_cjs.t[5][1] = "선물은…… 프로듀서에게……";
+window.im_cjs.t[5][2] = "주는 거야……. 나의…… 소중한";
+window.im_cjs.t[5][3] = "사람……. 고마워를……";
+window.im_cjs.t[5][4] = "많이…… 전하고 싶은 사람…….";
+
+window.im_cjs.t[6][1] = "그러면…… 안돼……? 프로듀서도";
+window.im_cjs.t[6][2] = "……나에게…… 고마워의";
+window.im_cjs.t[6][3] = "선물……? 그렇구나……";
+window.im_cjs.t[6][4] = "같은…… 기분이었어…….";
+
+window.im_cjs.t[7][1] = "그럼…… 어리광…… 들어줘";
+window.im_cjs.t[7][2] = "……. 손…… 꼭 잡고……";
+window.im_cjs.t[7][3] = "놓지 마……?";
+window.im_cjs.t[7][4] = "같이 나란히…… 가자……";
+window.im_cjs.t[7][5] = "응…….";
