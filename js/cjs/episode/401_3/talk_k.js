e76ca436eb177991a988a156d124b3e0698ed19c
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-20 23:22:15 +0900

    Create talk_k.js

---
+++ js/cjs/episode/401_3/talk_k.js
@@ -0,0 +1,41 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "기다리셨습니다~, 특제･나나의";
+window.im_cjs.t[1][2] = "메르헨 도시락입니다! 푹신푹신한 계란";
+window.im_cjs.t[1][3] = "말이에 사박사박한 가라아게, 주먹밥";
+window.im_cjs.t[1][4] = "이나 감자 샐러드도 있어요~!";
+
+window.im_cjs.t[2][1] = "그리고, 결정타는……메르헨･우사밍";
+window.im_cjs.t[2][2] = "비엔나~☆역시, 도시락에는";
+window.im_cjs.t[2][3] = "이거죠! 자자, 아무쪼록!";
+window.im_cjs.t[2][4] = "잔~뜩 드셔주세요♪";
+
+window.im_cjs.t[3][1] = "어라라? 나나의 요리솜씨에 놀라지";
+window.im_cjs.t[3][2] = "않나요? 이 정도는 할 수 있어요~.";
+window.im_cjs.t[3][3] = "폼으로 나이를 먹은게…… 아니,";
+window.im_cjs.t[3][4] = "훌륭한 우사밍 메이드니까요!";
+
+window.im_cjs.t[4][1] = "라니~. 꽤 일찍 일어나서";
+window.im_cjs.t[4][2] = "만들었는데요…… 에헤헤.";
+window.im_cjs.t[4][3] = "프로듀서 씨가 기뻐했으면 해서,";
+window.im_cjs.t[4][4] = "열심히 했습니다♪";
+
+window.im_cjs.t[5][1] = "프로듀서 씨는 언제나, 나나를";
+window.im_cjs.t[5][2] = "위해 정말 열심히 하시니까요. 오늘은";
+window.im_cjs.t[5][3] = "잔뜩 영양을 섭취하고, 느긋~";
+window.im_cjs.t[5][4] = "하게 치유되길 바라요.";
+
+window.im_cjs.t[6][1] = "그러니까 오늘은, 도시락을 먹으면서,";
+window.im_cjs.t[6][2] = "벌러덩 드러누워서, 일광욕도";
+window.im_cjs.t[6][3] = "해버리죠♪";
+window.im_cjs.t[6][4] = "늘쩡늘쩡, 대환영입니다!";
+
+window.im_cjs.t[7][1] = "결정이네요! 그럼, 처음은";
+window.im_cjs.t[7][2] = "도시락으로 하죠. 첫번째는,";
+window.im_cjs.t[7][3] = "나나가 먹여 드릴게요!";
+window.im_cjs.t[7][4] = "입을 벌리고……아~앙♪";
