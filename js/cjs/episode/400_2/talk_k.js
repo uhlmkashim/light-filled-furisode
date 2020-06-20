7903c474c5269ff1d4f67c737205552d1138765b
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-21 03:57:29 +0900

    Create talk_k.js

---
+++ js/cjs/episode/400_2/talk_k.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "프로듀서 씨도, 졸리세요?";
+window.im_cjs.t[1][2] = "……라니. 저, 아직 보육교사";
+window.im_cjs.t[1][3] = "기분이 가시지 않았네요. 굉장히";
+window.im_cjs.t[1][4] = "귀중한 체험이었으니까요.";
+
+window.im_cjs.t[2][1] = "그러니까…… 피곤하신 프로듀서";
+window.im_cjs.t[2][2] = "씨, 쿄코 선생님과 함께 낮잠";
+window.im_cjs.t[2][3] = "자지 않을래요? 모포를 덮어서,";
+window.im_cjs.t[2][4] = "자장가를 불러드릴게요♪";
+
+window.im_cjs.t[3][1] = "머리도, 풀어버렸으니까요.";
+window.im_cjs.t[3][2] = "자국이 생기면 안되고";
+window.im_cjs.t[3][3] = "……프로듀서 씨와의";
+window.im_cjs.t[3][4] = "곁잠도 자기 어려우니까요.";
+
+window.im_cjs.t[4][1] = "후훗, 신선해요? 사이드 테일은,";
+window.im_cjs.t[4][2] = "저의 챠밍포인트니까요. 집안일";
+window.im_cjs.t[4][3] = "하기 쉽고, 귀엽고…… 아이돌답고,";
+window.im_cjs.t[4][4] = "좋아해요.";
+
+window.im_cjs.t[5][1] = "하지만, 언제나의 저는 조금 휴식";
+window.im_cjs.t[5][2] = "이에요. 지금, 프로듀서 씨 곁에";
+window.im_cjs.t[5][3] = "있는 건, 함께 낮잠을 자고 싶은,";
+window.im_cjs.t[5][4] = "상냥한 쿄코 선생님이에요♪";
+
+window.im_cjs.t[6][1] = "그리고, 선생님은 알고 있어요.";
+window.im_cjs.t[6][2] = "프로듀서 씨라면, 알고";
+window.im_cjs.t[6][3] = "있다는 걸…… 꿈에서 깨어났을 때,";
+window.im_cjs.t[6][4] = "옆에 있는 것은, 저라고.";
+
+window.im_cjs.t[7][1] = "……프로듀서 씨, 잠드셨어요?";
+window.im_cjs.t[7][2] = "그럼…… 오늘도, 일";
+window.im_cjs.t[7][3] = "열심히 잘하셨어요♪";
+window.im_cjs.t[7][4] = "쓰담쓰담……";
+window.im_cjs.t[7][5] = "잘 자요♪";
