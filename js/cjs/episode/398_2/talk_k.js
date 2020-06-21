d4bce0a865305c418799690d2e551a1d2bee4ba2
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-21 14:56:56 +0900

    Create talk_k.js

---
+++ js/cjs/episode/398_2/talk_k.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "와, 눈……♪ 프로듀서 씨,";
+window.im_cjs.t[1][2] = "정말로 내리네요.";
+window.im_cjs.t[1][3] = "너무 예뻐……. 후후, 조금";
+window.im_cjs.t[1][4] = "넋을 잃고 봤어요.";
+
+window.im_cjs.t[2][1] = "올려다보면, 먹색의 하늘에서 결정";
+window.im_cjs.t[2][2] = "이 쏟아지고…… 주변도 조용해서……";
+window.im_cjs.t[2][3] = "마치 시간이 멈춘";
+window.im_cjs.t[2][4] = "것처럼 생각돼요.";
+
+window.im_cjs.t[3][1] = "하지만, 이 풍경은, 차갑고,";
+window.im_cjs.t[3][2] = "조금 쓸쓸한 기분도 들어요.";
+window.im_cjs.t[3][3] = "지금까지, 눈에는 즐거운 추억만";
+window.im_cjs.t[3][4] = "있었는데…… 신기해…….";
+
+window.im_cjs.t[4][1] = "앗, 죄송해요. 지금,";
+window.im_cjs.t[4][2] = "굉장히 즐거워요. 앞으로의";
+window.im_cjs.t[4][3] = "일도 기대되고, 곁에는";
+window.im_cjs.t[4][4] = "프로듀서 씨도 있으니까요.";
+
+window.im_cjs.t[5][1] = "하지만, 너무 행복하니까……";
+window.im_cjs.t[5][2] = "이 시간도, 눈처럼 덧없이";
+window.im_cjs.t[5][3] = "녹아버릴까봐, ";
+window.im_cjs.t[5][4] = "조금 애달파졌을지도…….";
+
+window.im_cjs.t[6][1] = "연인들의 마음도, 지금이라면";
+window.im_cjs.t[6][2] = "알 수 있어요. 분명, 행복한 시간을";
+window.im_cjs.t[6][3] = "덧없이 끝내고 싶지 않아,";
+window.im_cjs.t[6][4] = "느긋이 걷고 있던 거에요.";
+
+window.im_cjs.t[7][1] = "우리도, 꼭 붙어서,";
+window.im_cjs.t[7][2] = "천천히 걸어가죠.";
+window.im_cjs.t[7][3] = "차가운 설경도……";
+window.im_cjs.t[7][4] = "당신과 함께라면,";
+window.im_cjs.t[7][5] = "따뜻한 행복으로 변하니까요.";
