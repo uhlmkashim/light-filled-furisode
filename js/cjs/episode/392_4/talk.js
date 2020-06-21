9faf6cd014f848c0fdcb21a60f288f595bd082df
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-21 12:58:26 +0900

    Create talk.js

---
+++ js/cjs/episode/392_4/talk.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];﻿
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "あっ､ﾌﾟﾚｾﾞﾝﾄがある! ええっと";
+window.im_cjs.t[1][2] = "……Merry Christmas UZUKI……";
+window.im_cjs.t[1][3] = "卯月! わぁっ♪ありがとう";
+window.im_cjs.t[1][4] = "ございます､ﾌﾟﾛﾃﾞｭｰｻｰさん!";
+
+window.im_cjs.t[2][1] = "夜更かし禁止って､こういう";
+window.im_cjs.t[2][2] = "意味だったんですね｡ﾌﾟﾛﾃﾞｭｰｻｰ";
+window.im_cjs.t[2][3] = "さんが……ううん､ｻﾝﾀさんが";
+window.im_cjs.t[2][4] = "夜中に来てくれましたっ♪";
+
+window.im_cjs.t[3][1] = "朝起きたら､ﾂﾘｰの下にﾌﾟﾚｾﾞﾝﾄ";
+window.im_cjs.t[3][2] = "があるのって､ちょっぴり憧れ";
+window.im_cjs.t[3][3] = "てたんです｡前に映画で見て､";
+window.im_cjs.t[3][4] = "楽しそうだな～って｡えへへ｡";
+
+window.im_cjs.t[4][1] = "だから､本当にｻﾝﾀさんが来るな";
+window.im_cjs.t[4][2] = "んてすごいですっ♪ 想像して";
+window.im_cjs.t[4][3] = "いたよりもずーっとずっと素敵";
+window.im_cjs.t[4][4] = "で､感動しちゃいました!";
+
+window.im_cjs.t[5][1] = "それに､ｻﾝﾀさんからだけじゃな";
+window.im_cjs.t[5][2] = "くて､ﾌﾟﾛﾃﾞｭｰｻｰさんからも素敵";
+window.im_cjs.t[5][3] = "なﾌﾟﾚｾﾞﾝﾄを貰えましたし｡";
+window.im_cjs.t[5][4] = "えへへ､なんだかわかりますか?";
+
+window.im_cjs.t[6][1] = "答えは､笑顔ですっ♪ﾌﾟﾛﾃﾞｭｰｻｰ";
+window.im_cjs.t[6][2] = "さんと迎えたｸﾘｽﾏｽは､優しくて､";
+window.im_cjs.t[6][3] = "ｷﾗｷﾗしてて……とびきりの笑顔";
+window.im_cjs.t[6][4] = "になれました!";
+
+window.im_cjs.t[7][1] = "ﾌﾟﾛﾃﾞｭｰｻｰさんも､素敵な笑顔だ";
+window.im_cjs.t[7][2] = "から､きっと同じ気持ち……";
+window.im_cjs.t[7][3] = "かな? じゃあ私からも､大切に､";
+window.im_cjs.t[7][4] = "心をこめて……";
+window.im_cjs.t[7][5] = "ﾒﾘｰｸﾘｽﾏｽ♪";
