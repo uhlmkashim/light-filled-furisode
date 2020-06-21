a7b62b2d769b2893cda63494f484a7e9bdba1a98
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-21 13:00:55 +0900

    Create talk.js

---
+++ js/cjs/episode/398_2/talk.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "わぁ､雪……♪ﾌﾟﾛﾃﾞｭｰｻｰさん､";
+window.im_cjs.t[1][2] = "本当に降ってきましたよ｡";
+window.im_cjs.t[1][3] = "すごく綺麗……｡ふふ､少し";
+window.im_cjs.t[1][4] = "見惚れちゃいました｡";
+
+window.im_cjs.t[2][1] = "見上げたら､鈍色の空から結晶";
+window.im_cjs.t[2][2] = "が降り注いできて……周りも静";
+window.im_cjs.t[2][3] = "かで……まるで時が止まってい";
+window.im_cjs.t[2][4] = "るように思うんです｡";
+
+window.im_cjs.t[3][1] = "でも､この景色は､冷たくて､";
+window.im_cjs.t[3][2] = "少し寂しい気もします｡今まで､";
+window.im_cjs.t[3][3] = "雪には楽しい思い出しかなかっ";
+window.im_cjs.t[3][4] = "たのに……不思議……｡";
+
+window.im_cjs.t[4][1] = "あっ､すみません｡今､すごく";
+window.im_cjs.t[4][2] = "楽しいんですよ｡この後のお";
+window.im_cjs.t[4][3] = "仕事も楽しみですし､隣には";
+window.im_cjs.t[4][4] = "ﾌﾟﾛﾃﾞｭｰｻｰさんもいますから｡";
+
+window.im_cjs.t[5][1] = "でも､あまりにも幸せだから";
+window.im_cjs.t[5][2] = "……この時間も､雪みたいに儚";
+window.im_cjs.t[5][3] = "く溶けちゃうんじゃないかって､";
+window.im_cjs.t[5][4] = "少し切なくなったのかも……｡";
+
+window.im_cjs.t[6][1] = "恋人たちの気持ちも､今ならわ";
+window.im_cjs.t[6][2] = "かります｡きっと､幸せな時間を";
+window.im_cjs.t[6][3] = "儚く終わらせたくなくて､";
+window.im_cjs.t[6][4] = "ゆっくり歩いていたんですね｡";
+
+window.im_cjs.t[7][1] = "私たちも､寄り添いながら､";
+window.im_cjs.t[7][2] = "ゆっくり歩いていきましょう｡";
+window.im_cjs.t[7][3] = "冷たい雪景色も……";
+window.im_cjs.t[7][4] = "貴方となら､";
+window.im_cjs.t[7][5] = "温かな幸せに変わりますから｡";
