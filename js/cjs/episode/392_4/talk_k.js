264aaba6c9611f625e16f35cc848a751c57c91e8
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-21 13:49:24 +0900

    Create talk_k.js

---
+++ js/cjs/episode/392_4/talk_k.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];﻿
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "앗, 선물이 있어! 그러니까……";
+window.im_cjs.t[1][2] = "Merry Christmas UZUKI……";
+window.im_cjs.t[1][3] = "우즈키! 와앗♪ 감사합니다,";
+window.im_cjs.t[1][4] = "프로듀서 씨!";
+
+window.im_cjs.t[2][1] = "밤샘금지라는 게, 이런";
+window.im_cjs.t[2][2] = "이런 의미였군요. 프로듀서";
+window.im_cjs.t[2][3] = "씨가…… 아니, 산타 씨가";
+window.im_cjs.t[2][4] = "밤중에 와주셨어요♪";
+
+window.im_cjs.t[3][1] = "아침에 일어나니, 트리 밑에 선물";
+window.im_cjs.t[3][2] = "이 있다는 거, 조금 동경하고";
+window.im_cjs.t[3][3] = "있었어요. 전에 영화에서 보니,";
+window.im_cjs.t[3][4] = "재밌을것 같아~하고요. 에헤헤.";
+
+window.im_cjs.t[4][1] = "그러니까, 정말로 산타 씨가";
+window.im_cjs.t[4][2] = "와주다니 대단해요♪ 상상했던";
+window.im_cjs.t[4][3] = "것보다 훨~씬 훨씬 멋져서,";
+window.im_cjs.t[4][4] = "감동받았어요!";
+
+window.im_cjs.t[5][1] = "게다가, 산타 씨로부터만이";
+window.im_cjs.t[5][2] = "아니라, 프로듀서 씨로부터도 멋진";
+window.im_cjs.t[5][3] = "선물을 받았구요.";
+window.im_cjs.t[5][4] = "에헤헤, 뭔지 아시겠어요?";
+
+window.im_cjs.t[6][1] = "답은, 미소입니다♪ 프로듀서";
+window.im_cjs.t[6][2] = "씨와 맞이한 크리스마스는, 상냥하고,";
+window.im_cjs.t[6][3] = "반짝반짝해서…… 특별한 미소를";
+window.im_cjs.t[6][4] = "지을 수 있었어요!";
+
+window.im_cjs.t[7][1] = "프로듀서 씨도, 멋진 미소니까,";
+window.im_cjs.t[7][2] = "분명 같은 기분……";
+window.im_cjs.t[7][3] = "일까나? 그럼 저로부터도, 소중하게,";
+window.im_cjs.t[7][4] = "마음을 담아……";
+window.im_cjs.t[7][5] = "메리 크리스마스♪";
