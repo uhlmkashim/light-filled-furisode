5510aea1324c5700c65680428ca51acad656df7e
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-06-21 13:58:19 +0900

    Create talk_k.js

---
+++ js/cjs/episode/397_3/talk_k.js
@@ -0,0 +1,41 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "役者は揃い……今､刻が満ちた! ";
+window.im_cjs.t[1][2] = "……はぁ､間に合ってよかっ";
+window.im_cjs.t[1][3] = "たぁ……! 早速､儀式を執り";
+window.im_cjs.t[1][4] = "行うわ! さぁ……いでよっ!";
+
+window.im_cjs.t[2][1] = "闇より賜いし､我が至宝! この";
+window.im_cjs.t[2][2] = "瞬間から､所有権は友へと移る! ";
+window.im_cjs.t[2][3] = "(ﾁｮｺﾚｰﾄを作ってきたんです……";
+window.im_cjs.t[2][4] = "受け取ってください!)";
+
+window.im_cjs.t[3][1] = "秘めたる力は解放され､数多の";
+window.im_cjs.t[3][2] = "屍の上に､創造を果たしたわ";
+window.im_cjs.t[3][3] = "……｡(たくさん練習したから､";
+window.im_cjs.t[3][4] = "きっと美味しくできたと思うの)";
+
+window.im_cjs.t[4][1] = "混沌､破壊､そして､灼熱の業火";
+window.im_cjs.t[4][2] = "……我が友を想えば､どれも戯";
+window.im_cjs.t[4][3] = "れにすぎず! (喜ぶ顔が見たく";
+window.im_cjs.t[4][4] = "て､頑張りました!)";
+
+window.im_cjs.t[5][1] = "えっと……貴方に､ありがとう";
+window.im_cjs.t[5][2] = "の気持ちと､これからもお願い";
+window.im_cjs.t[5][3] = "しますの想いを伝えたくて";
+window.im_cjs.t[5][4] = "……手作りしてみたの｡";
+
+window.im_cjs.t[6][1] = "……うぅ､まだ業火に焼かれて";
+window.im_cjs.t[6][2] = "いるように､頬が熱い……けれ";
+window.im_cjs.t[6][3] = "ど､これで､我らの盟約は永遠の";
+window.im_cjs.t[6][4] = "ものとなった! えへへ……♪";
+
+window.im_cjs.t[7][1] = "……ｺﾎﾝ｡我が友には……大切な､";
+window.im_cjs.t[7][2] = "この言葉を贈るわ! ﾊｯﾋﾟｰﾊﾞﾚﾝﾀ";
+window.im_cjs.t[7][3] = "ｲﾝ､ﾌﾟﾛﾃﾞｭｰｻｰ……";
+window.im_cjs.t[7][4] = "親愛なる貴方に､心を込めて♪";
