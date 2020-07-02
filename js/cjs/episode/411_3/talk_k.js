8b0a31222e8a7e4adc386963d57eae5d2a1c1c19
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-07-03 00:43:12 +0900

    Create talk_k.js

---
+++ js/cjs/episode/411_3/talk_k.js
@@ -0,0 +1,41 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "와…… 예뻐……. 정말로,";
+window.im_cjs.t[1][2] = "이렇게 멋진 경치야…….";
+window.im_cjs.t[1][3] = "굉장하구나…… 판타지 세계에";
+window.im_cjs.t[1][4] = "들어간 것 같아.";
+
+window.im_cjs.t[2][1] = "이곳은, 세계유산으로, 유명";
+window.im_cjs.t[2][2] = "한 거네요~. TV에서는 본";
+window.im_cjs.t[2][3] = "적이 있었지만…… 실제로";
+window.im_cjs.t[2][4] = "보면, 전혀 다르네요.";
+
+window.im_cjs.t[3][1] = "프로듀서 씨는, 역시 대단해요!";
+window.im_cjs.t[3][2] = "처음 만났을 때부터,";
+window.im_cjs.t[3][3] = "쭉 그래요♪ 대단한 것을, 펑";
+window.im_cjs.t[3][4] = "하고 해주시는 거예요.";
+
+window.im_cjs.t[4][1] = "마치, 저 기구처럼. 둥실,";
+window.im_cjs.t[4][2] = "아무것도 아닌 것처럼 저를";
+window.im_cjs.t[4][3] = "데려가서, 그리고 멋진";
+window.im_cjs.t[4][4] = "경치를 보여줘요!";
+
+window.im_cjs.t[5][1] = "그렇게 해서 저는, 점점 멀리,";
+window.im_cjs.t[5][2] = "점점 높은 곳에 와 있는";
+window.im_cjs.t[5][3] = "거네요~. 말로 할 수 없을 정도로";
+window.im_cjs.t[5][4] = "아름다운 곳에……♪";
+
+window.im_cjs.t[6][1] = "그건, 제가 보통 여자애인";
+window.im_cjs.t[6][2] = "채로는 할 수 없는 일이니까요!";
+window.im_cjs.t[6][3] = "프로듀서 씨의 힘으로 특별한";
+window.im_cjs.t[6][4] = "체험을 하고 있어요.";
+
+window.im_cjs.t[7][1] = "어라? 저, 이상한 말을";
+window.im_cjs.t[7][2] = "했나요? 그래도, 제대로 말로";
+window.im_cjs.t[7][3] = "하고 싶으니까요! 프로듀서 씨,";
+window.im_cjs.t[7][4] = "언제나 감사합니다♪";
