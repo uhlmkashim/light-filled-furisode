24b15ecbe6d6979c6e4dc434b88296cf4a337080
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-07-03 00:20:47 +0900

    Create talk.js

---
+++ js/cjs/episode/411_3/talk.js
@@ -0,0 +1,41 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "わぁ……綺麗……｡本当に､こん";
+window.im_cjs.t[1][2] = "なに素敵な景色なんだぁ……｡";
+window.im_cjs.t[1][3] = "すごいなぁ……ﾌｧﾝﾀｼﾞｰの世界";
+window.im_cjs.t[1][4] = "に入っちゃったみたい｡";
+
+window.im_cjs.t[2][1] = "ここって､世界遺産として､有名";
+window.im_cjs.t[2][2] = "なんですよね～｡ﾃﾚﾋﾞでは見た";
+window.im_cjs.t[2][3] = "ことありましたけど……生で";
+window.im_cjs.t[2][4] = "見ると､全然違いますねっ｡";
+
+window.im_cjs.t[3][1] = "ﾌﾟﾛﾃﾞｭｰｻｰさんって､やっぱりす";
+window.im_cjs.t[3][2] = "ごいです! 出会ったときから､";
+window.im_cjs.t[3][3] = "ずっとそう♪すごいことを､ぽ";
+window.im_cjs.t[3][4] = "んってやってくれるんです｡";
+
+window.im_cjs.t[4][1] = "まるで､あの気球みたいに｡ふわ";
+window.im_cjs.t[4][2] = "り､何でもないかのように私を";
+window.im_cjs.t[4][3] = "連れて行って､そして素敵な景";
+window.im_cjs.t[4][4] = "色を見せてくれてっ!";
+
+window.im_cjs.t[5][1] = "そうやって私は､どんどん遠く､";
+window.im_cjs.t[5][2] = "どんどん高いところに来てるん";
+window.im_cjs.t[5][3] = "ですよね～｡言葉にできないく";
+window.im_cjs.t[5][4] = "らい美しいところに……♪";
+
+window.im_cjs.t[6][1] = "それって､私が普通の女の子の";
+window.im_cjs.t[6][2] = "ままじゃできなかったことです";
+window.im_cjs.t[6][3] = "から! ﾌﾟﾛﾃﾞｭｰｻｰさんの力で特";
+window.im_cjs.t[6][4] = "別な体験をしてるんですっ｡";
+
+window.im_cjs.t[7][1] = "あれっ? 私､おかしなこと言い";
+window.im_cjs.t[7][2] = "ました? でも､ちゃんと言葉に";
+window.im_cjs.t[7][3] = "したいから! ﾌﾟﾛﾃﾞｭｰｻｰさん､";
+window.im_cjs.t[7][4] = "いつもありがとうございます♪";
