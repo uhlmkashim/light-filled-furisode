c2c69d164b00fbea5bc3cf36b66d9f4df815e775
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-07-03 23:57:02 +0900

    Create talk_k.js

---
+++ js/cjs/episode/383_2/talk_k.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];﻿
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "음…… 좋은 날♪이렇게 프로듀서";
+window.im_cjs.t[1][2] = "씨와 어깨를 나란히, 느긋이";
+window.im_cjs.t[1][3] = "산책할 수 있는걸.";
+window.im_cjs.t[1][4] = "마유, 너무 행복해……♪";
+
+window.im_cjs.t[2][1] = "가로수도, 붉게 물들어서, 예뻐";
+window.im_cjs.t[2][2] = "……♪ 분명, 가을은 짧으니까";
+window.im_cjs.t[2][3] = "…… 소중한 사람에게 보여줄 수";
+window.im_cjs.t[2][4] = "있도록, 열심히 물들었을지도.";
+
+window.im_cjs.t[3][1] = "저기, 프로듀서 씨…… 기모노";
+window.im_cjs.t[3][2] = "차림의 마유, 마음에 드셨어요?";
+window.im_cjs.t[3][3] = "평소보다, 어른스러운 마유";
+window.im_cjs.t[3][4] = "…… 의식해 봤어요♪";
+
+window.im_cjs.t[4][1] = "우후훗, 다행이다……♪프로듀서";
+window.im_cjs.t[4][2] = "씨가 원한다면, 어떤";
+window.im_cjs.t[4][3] = "마유라도 될 수 있지만……";
+window.im_cjs.t[4][4] = "오늘은, 강하게 생각했어요.";
+
+window.im_cjs.t[5][1] = "어째서일까……. 평상시엔 입지";
+window.im_cjs.t[5][2] = "않는, 기모노니까? 머리를 묶고";
+window.im_cjs.t[5][3] = "있어서? 아니면…… 조금";
+window.im_cjs.t[5][4] = "애달픈 가을, 그래서?";
+
+window.im_cjs.t[6][1] = "이유는 알 수 없어요. 하지만……";
+window.im_cjs.t[6][2] = "어느 것도, 사소한 일이네요.";
+window.im_cjs.t[6][3] = "빠르게 지나가는 계절을, 당신과";
+window.im_cjs.t[6][4] = "보낼 수 있는 기쁨과 비교하면♪";
+
+window.im_cjs.t[7][1] = "그러니까, 프로듀서 씨. 조금만";
+window.im_cjs.t[7][2] = "더…… 마유와, 한가로이,";
+window.im_cjs.t[7][3] = "걷지 않으실래요?";
+window.im_cjs.t[7][4] = "두 사람의 시간이,";
+window.im_cjs.t[7][5] = "계~속 이어지도록……♪";
