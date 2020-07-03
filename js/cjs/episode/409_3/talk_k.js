4690526cc412849dd3f5f5a708230447bfed5f42
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-07-03 23:26:22 +0900

    Update talk_k.js

--- js/cjs/episode/409_3/talk_k.js
+++ js/cjs/episode/409_3/talk_k.js
@@ -15,8 +15,8 @@ window.im_cjs.t[2][2] = "부질없이 잠만 자는 마유는,";
 window.im_cjs.t[2][3] = "이제 어디에도 없어. 왜냐하면";
 window.im_cjs.t[2][4] = "……지금은, 알고 있으니까)";
 
-window.im_cjs.t[3][1] = "(……그 사람은, 곧 여기에";
-window.im_cjs.t[3][2] = "와 줄거야. 그리고, 맹세해 줄거야.";
+window.im_cjs.t[3][1] = "(……그 사람은, 곧 여기에 와";
+window.im_cjs.t[3][2] = "줄거야. 그리고, 맹세해 줄거야.";
 window.im_cjs.t[3][3] = "마유와 함께, 계속 계속,";
 window.im_cjs.t[3][4] = "걸어줄 거라고)";
 
@@ -25,7 +25,7 @@ window.im_cjs.t[4][2] = "최초에 마음에 그린 인연과는,";
 window.im_cjs.t[4][3] = "다르지만…… 그래도, 그걸로 좋아";
 window.im_cjs.t[4][4] = "……아니. 그게 좋아)";
 
-window.im_cjs.t[5][1] = "(가장 가까이 있을 뿐만이 아니라…….";
+window.im_cjs.t[5][1] = "(가장 가까이 있을 뿐 아니라…….";
 window.im_cjs.t[5][2] = "당신이 말하는 미래를, 당신이";
 window.im_cjs.t[5][3] = "바라는 미래를, 이룰 수 있어.";
 window.im_cjs.t[5][4] = "마유가, 이뤄줄 수 있어)";
