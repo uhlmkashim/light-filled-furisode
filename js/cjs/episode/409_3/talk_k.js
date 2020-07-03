029a114d1b49286a3328360f36693778350c67e2
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-07-03 23:21:40 +0900

    Update talk_k.js

--- js/cjs/episode/409_3/talk_k.js
+++ js/cjs/episode/409_3/talk_k.js
@@ -16,22 +16,22 @@ window.im_cjs.t[2][3] = "이제 어디에도 없어. 왜냐하면";
 window.im_cjs.t[2][4] = "……지금은, 알고 있으니까)";
 
 window.im_cjs.t[3][1] = "(……그 사람은, 곧 여기에";
-window.im_cjs.t[3][2] = "와 줄거야. 그리고, 맹세해 줄거";
-window.im_cjs.t[3][3] = "야. 마유와 함께, 계속 계속,";
+window.im_cjs.t[3][2] = "와 줄거야. 그리고, 맹세해 줄거야.";
+window.im_cjs.t[3][3] = "마유와 함께, 계속 계속,";
 window.im_cjs.t[3][4] = "걸어줄 거라고)";
 
 window.im_cjs.t[4][1] = "(아이돌과 프로듀서. 그 미래는,";
-window.im_cjs.t[4][2] = "최초에 마음에 그린 인연과는, 다르";
-window.im_cjs.t[4][3] = "지만…… 그래도, 그걸로 좋아";
+window.im_cjs.t[4][2] = "최초에 마음에 그린 인연과는,";
+window.im_cjs.t[4][3] = "다르지만…… 그래도, 그걸로 좋아";
 window.im_cjs.t[4][4] = "……아니. 그게 좋아)";
 
-window.im_cjs.t[5][1] = "(가장 가까이 있을 뿐만이 아";
-window.im_cjs.t[5][2] = "니라……. 당신이 말하는 미래를, 당";
-window.im_cjs.t[5][3] = "신이 바라는 미래를, 이룰 수 있어.";
+window.im_cjs.t[5][1] = "(가장 가까이 있을 뿐만이 아니라…….";
+window.im_cjs.t[5][2] = "당신이 말하는 미래를, 당신이";
+window.im_cjs.t[5][3] = "바라는 미래를, 이룰 수 있어.";
 window.im_cjs.t[5][4] = "마유가, 이뤄줄 수 있어)";
 
-window.im_cjs.t[6][1] = "……우후후, 역시 와주셨네";
-window.im_cjs.t[6][2] = "요, 프로듀서 씨. 그럼……";
+window.im_cjs.t[6][1] = "……우후후, 역시 와주셨네요,";
+window.im_cjs.t[6][2] = "프로듀서 씨. 그럼……";
 window.im_cjs.t[6][3] = "부디, 마유에게 맹세해 주세요.";
 window.im_cjs.t[6][4] = "당신의, 그 말로.";
 
