00d8233df0fda0b708e5268a18aaa3599e0b6e1d
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-07-09 19:07:00 +0900

    Create talk_k.js

---
+++ js/cjs/episode/412_2/talk_k.js
@@ -0,0 +1,41 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "빨리 가자, 프로듀서 씨!";
+window.im_cjs.t[1][2] = "오늘은, 어떤 만남이 있을까나?";
+window.im_cjs.t[1][3] = "상상만 해도 두근두근해.";
+window.im_cjs.t[1][4] = "엄청, 기대돼♪";
+
+window.im_cjs.t[2][1] = "아이돌이 되어서, 많은 일을";
+window.im_cjs.t[2][2] = "경험해서…… 매일이 정말 즐거워.";
+window.im_cjs.t[2][3] = "새로운 것을 보는 걸,";
+window.im_cjs.t[2][4] = "쭉 동경해왔고.";
+
+window.im_cjs.t[3][1] = "게다가, 이렇게 돌아보면,";
+window.im_cjs.t[3][2] = "프로듀서 씨가 지켜봐주고 있어.";
+window.im_cjs.t[3][3] = "만약 터무니없는 일이 되어도,";
+window.im_cjs.t[3][4] = "지탱해줘. 그치? 후훗.";
+
+window.im_cjs.t[4][1] = "지금까지의 내가 옳았으니까,";
+window.im_cjs.t[4][2] = "100점 만점이었으니까 지금이 있다";
+window.im_cjs.t[4][3] = "…… 아니. 그게 아냐.";
+window.im_cjs.t[4][4] = "그런 게 아니야. 나, 생각했어.";
+
+window.im_cjs.t[5][1] = "분명 틀린 것도 잔뜩 있어.";
+window.im_cjs.t[5][2] = "생각도, 기세로 나온, 예민한";
+window.im_cjs.t[5][3] = "말도. ……언제나 좋은 아이로";
+window.im_cjs.t[5][4] = "있는다든가, 무리고.";
+
+window.im_cjs.t[6][1] = "하지만, 나에게는 프로듀서 씨가";
+window.im_cjs.t[6][2] = "있어. 안될 때는 꾸짖어 주고, 노력";
+window.im_cjs.t[6][3] = "하면 칭찬해줘. 내가 걷고 싶은 ";
+window.im_cjs.t[6][4] = "길을, 함께 걸어줘.";
+
+window.im_cjs.t[7][1] = "그래서 나는, 안심하고, 아이돌";
+window.im_cjs.t[7][2] = "호죠 카렌으로 있을 수 있어. 자신";
+window.im_cjs.t[7][3] = "을 관철하는 일이 가능한 거야.";
+window.im_cjs.t[7][4] = "당신이…… 있으니까.";
