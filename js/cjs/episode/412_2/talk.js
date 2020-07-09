b9e737d72f0d693228c2f1487f502f72f9ba7487
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-07-09 18:40:02 +0900

    Create talk.js

---
+++ js/cjs/episode/412_2/talk.js
@@ -0,0 +1,41 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "早く行こうよ､ﾌﾟﾛﾃﾞｭｰｻｰさんっ!";
+window.im_cjs.t[1][2] = "今日は､どんな出逢いがあるか";
+window.im_cjs.t[1][3] = "な? 想像しただけでﾜｸﾜｸする｡";
+window.im_cjs.t[1][4] = "すっごく､楽しみなの♪";
+
+window.im_cjs.t[2][1] = "ｱｲﾄﾞﾙになって､いろんなことを";
+window.im_cjs.t[2][2] = "経験して……毎日が本当に楽し";
+window.im_cjs.t[2][3] = "いんだ｡新しいものを見ること";
+window.im_cjs.t[2][4] = "に､ずっと憧れてたし｡";
+
+window.im_cjs.t[3][1] = "それに､こうして振り向けば､";
+window.im_cjs.t[3][2] = "ﾌﾟﾛﾃﾞｭｰｻｰさんは見てくれてる｡";
+window.im_cjs.t[3][3] = "もし無茶しそうになっても､";
+window.im_cjs.t[3][4] = "支えてくれる｡でしょ? ふふっ｡";
+
+window.im_cjs.t[4][1] = "今までの私が正しかったから､";
+window.im_cjs.t[4][2] = "100点満点だったから今がある";
+window.im_cjs.t[4][3] = "……ううん｡そうじゃない｡";
+window.im_cjs.t[4][4] = "そんなことない｡私､思ったんだ｡";
+
+window.im_cjs.t[5][1] = "きっと間違ってたこともいっぱ";
+window.im_cjs.t[5][2] = "いある｡考えも､勢いで出た､尖っ";
+window.im_cjs.t[5][3] = "た言葉も｡……いつもｲｲ子で";
+window.im_cjs.t[5][4] = "いるとか､無理だし｡";
+
+window.im_cjs.t[6][1] = "でも､私にはﾌﾟﾛﾃﾞｭｰｻｰさんがい";
+window.im_cjs.t[6][2] = "る｡ﾀﾞﾒな時は叱って､頑張った";
+window.im_cjs.t[6][3] = "ら褒めてくれる｡私が歩きたい";
+window.im_cjs.t[6][4] = "道を､一緒に歩いてくれる｡";
+
+window.im_cjs.t[7][1] = "だから私は､安心して､ｱｲﾄﾞﾙ";
+window.im_cjs.t[7][2] = "北条加蓮でいられるんだ｡自分";
+window.im_cjs.t[7][3] = "を貫くことが､できるんだよね｡";
+window.im_cjs.t[7][4] = "あなたが……いるから｡";
