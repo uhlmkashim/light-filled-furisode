5ab23b67dcdcd6b4e8e08ef5ce62ae57370e1d31
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-07-31 20:43:01 +0900

    Create talk.js

---
+++ js/cjs/episode/413_4/talk.js
@@ -0,0 +1,43 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "ふぅ～……ちょっと休憩です♪";
+window.im_cjs.t[1][2] = "とっても綺麗なﾌﾟｰﾙですよね｡";
+window.im_cjs.t[1][3] = "ﾗｲﾄｱｯﾌﾟされて､まるで星空の";
+window.im_cjs.t[1][4] = "中を泳いでるみたいでした!";
+
+window.im_cjs.t[2][1] = "ﾌﾟｰﾙも､あっちに見える夜景も";
+window.im_cjs.t[2][2] = "素敵ですし……ｼﾞｭｰｽも美味し";
+window.im_cjs.t[2][3] = "くって! 少しﾋﾞﾀｰで甘酸っぱい､";
+window.im_cjs.t[2][4] = "大人のﾚﾓﾈｰﾄﾞですよね｡";
+
+window.im_cjs.t[3][1] = "今日の私も､少しは大人っぽく";
+window.im_cjs.t[3][2] = "なれたでしょうか? えへへ､";
+window.im_cjs.t[3][3] = "ありがとうございます｡水着､";
+window.im_cjs.t[3][4] = "頑張って新調してよかった……｡";
+
+window.im_cjs.t[4][1] = "これからの私は､どんなｱｲﾄﾞﾙに";
+window.im_cjs.t[4][2] = "なれるのかなって､それをお話";
+window.im_cjs.t[4][3] = "したくて､たくさん考えたんで";
+window.im_cjs.t[4][4] = "すけど……｡";
+
+window.im_cjs.t[5][1] = "可愛いのも､かっこいいのも､";
+window.im_cjs.t[5][2] = "元気なのも……大人っぽい私も";
+window.im_cjs.t[5][3] = "素敵だと思うんです｡色々考え";
+window.im_cjs.t[5][4] = "てたら､どれも良くって｡";
+
+window.im_cjs.t[6][1] = "けど､きっと……｡どんな私で";
+window.im_cjs.t[6][2] = "あっても､私が一番したいこと";
+window.im_cjs.t[6][3] = "は……これからも､ﾌｧﾝのみな";
+window.im_cjs.t[6][4] = "さんを笑顔にすること!";
+
+window.im_cjs.t[7][1] = "えへへ……♪私の夢､ﾌﾟﾛﾃﾞｭｰｻｰ";
+window.im_cjs.t[7][2] = "さんにまた伝えられて､よかっ";
+window.im_cjs.t[7][3] = "たです♪";
+
+window.im_cjs.t[7][5] = "これからも､";
+window.im_cjs.t[7][6] = "私と一緒に歩いてくださいね!";
