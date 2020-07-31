a167046fc02a83730d1b5b102fb4357825b1b164
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-07-31 22:15:50 +0900

    Update talk_k.js

--- js/cjs/episode/413_4/talk_k.js
+++ js/cjs/episode/413_4/talk_k.js
@@ -11,7 +11,7 @@ window.im_cjs.t[1][3] = "라이트업돼서, 마치 별하늘";
 window.im_cjs.t[1][4] = "속을 헤엄치는 것 같았어요!";
 
 window.im_cjs.t[2][1] = "풀도, 저쪽에 보이는 야경도";
-window.im_cjs.t[2][2] = "멋지고……주스도 맛있고!";
+window.im_cjs.t[2][2] = "멋지고…… 주스도 맛있고!";
 window.im_cjs.t[2][3] = "조금 씁쓸하고 새콤달콤한,";
 window.im_cjs.t[2][4] = "어른의 레모네이드네요.";
 
@@ -28,7 +28,7 @@ window.im_cjs.t[4][4] = "했는데요…….";
 window.im_cjs.t[5][1] = "귀여운 것도, 멋진 것도,";
 window.im_cjs.t[5][2] = "건강한 것도…… 어른스러운 저도";
 window.im_cjs.t[5][3] = "멋지다고 생각해요. 여러가지 생각해";
-window.im_cjs.t[5][4] = "보면, 어느것이나 좋아서요.";
+window.im_cjs.t[5][4] = "보니, 어느것이나 좋아서요.";
 
 window.im_cjs.t[6][1] = "하지만, 분명……. 어떤 제가";
 window.im_cjs.t[6][2] = "있더라도, 제가 가장 하고 싶은 일은";
