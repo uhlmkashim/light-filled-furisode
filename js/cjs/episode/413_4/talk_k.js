8cc8a3f55ab63ac90e14b5ab0c3cd493712b341a
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-07-31 22:10:50 +0900

    Create talk_k.js

---
+++ js/cjs/episode/413_4/talk_k.js
@@ -0,0 +1,43 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "후우~…… 잠깐 휴식이에요♪";
+window.im_cjs.t[1][2] = "무척 예쁜 풀이네요.";
+window.im_cjs.t[1][3] = "라이트업돼서, 마치 별하늘";
+window.im_cjs.t[1][4] = "속을 헤엄치는 것 같았어요!";
+
+window.im_cjs.t[2][1] = "풀도, 저쪽에 보이는 야경도";
+window.im_cjs.t[2][2] = "멋지고……주스도 맛있고!";
+window.im_cjs.t[2][3] = "조금 씁쓸하고 새콤달콤한,";
+window.im_cjs.t[2][4] = "어른의 레모네이드네요.";
+
+window.im_cjs.t[3][1] = "오늘의 저도, 조금은 어른스러워";
+window.im_cjs.t[3][2] = "졌을까요? 에헤헤,";
+window.im_cjs.t[3][3] = "감사합니다. 수영복,";
+window.im_cjs.t[3][4] = "힘내서 새로 맞춰서 다행이다…….";
+
+window.im_cjs.t[4][1] = "앞으로의 저는, 어떤 아이돌이";
+window.im_cjs.t[4][2] = "될 수 있을까 하고, 그걸 얘기";
+window.im_cjs.t[4][3] = "하고 싶어서, 많은 생각을";
+window.im_cjs.t[4][4] = "했는데요…….";
+
+window.im_cjs.t[5][1] = "귀여운 것도, 멋진 것도,";
+window.im_cjs.t[5][2] = "건강한 것도…… 어른스러운 저도";
+window.im_cjs.t[5][3] = "멋지다고 생각해요. 여러가지 생각해";
+window.im_cjs.t[5][4] = "보면, 어느것이나 좋아서요.";
+
+window.im_cjs.t[6][1] = "하지만, 분명……. 어떤 제가";
+window.im_cjs.t[6][2] = "있더라도, 제가 가장 하고 싶은 일은";
+window.im_cjs.t[6][3] = "…… 앞으로도, 팬 여러분을";
+window.im_cjs.t[6][4] = "미소짓게 하는 것!";
+
+window.im_cjs.t[7][1] = "에헤헤……♪저의 꿈, 프로듀서";
+window.im_cjs.t[7][2] = "씨에게 전해져서,";
+window.im_cjs.t[7][3] = "다행이에요♪";
+
+window.im_cjs.t[7][5] = "앞으로도,";
+window.im_cjs.t[7][6] = "저와 함께 걸어주세요!";
