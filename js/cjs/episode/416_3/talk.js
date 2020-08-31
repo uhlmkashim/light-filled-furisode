0d9f0a3c03d37d52448cd6afd2901c10e4ce253a
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-08-31 16:10:50 +0900

    Create talk.js

---
+++ js/cjs/episode/416_3/talk.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "ほら､いかが? 上手に書けまし";
+window.im_cjs.t[1][2] = "たわ｡完璧な状態でﾌﾟﾛﾃﾞｭｰｻｰ";
+window.im_cjs.t[1][3] = "ちゃまにお出しするために､";
+window.im_cjs.t[1][4] = "密かに練習しましたのよ♪";
+
+window.im_cjs.t[2][1] = "はじめは驚きましたの｡ｵﾑﾗｲｽに､";
+window.im_cjs.t[2][2] = "絵や言葉をのせる文化があった";
+window.im_cjs.t[2][3] = "だなんて｡わたくし､まだまだ勉";
+window.im_cjs.t[2][4] = "強不足ですわね｡";
+
+window.im_cjs.t[3][1] = "何を書こうか､とても悩みまし";
+window.im_cjs.t[3][2] = "たし｡でも､せっかくですもの｡";
+window.im_cjs.t[3][3] = "ここはやはり､ｽﾄﾚｰﾄな想いを";
+window.im_cjs.t[3][4] = "お伝えしたくて｡";
+
+window.im_cjs.t[4][1] = "本当は､ｵﾑﾗｲｽも作って差し上げ";
+window.im_cjs.t[4][2] = "たかったのですけど……撮影に";
+window.im_cjs.t[4][3] = "使用するものですし､ﾌﾟﾛの方に";
+window.im_cjs.t[4][4] = "お任せしましたわ｡";
+
+window.im_cjs.t[5][1] = "その代わり､ｵﾑﾗｲｽの仕上げは";
+window.im_cjs.t[5][2] = "無しにしていただきましたのよ｡";
+window.im_cjs.t[5][3] = "皆さま､首を傾げていらしたわ｡";
+window.im_cjs.t[5][4] = "";
+
+window.im_cjs.t[6][1] = "でも､理由は秘密にしましたの｡";
+window.im_cjs.t[6][2] = "だって､ﾌﾟﾛﾃﾞｭｰｻｰちゃまに";
+window.im_cjs.t[6][3] = "ﾒｯｾｰｼﾞを贈るためだなんて……";
+window.im_cjs.t[6][4] = "みなさんには言えませんわ｡";
+
+window.im_cjs.t[7][1] = "ですから､ﾌﾟﾛﾃﾞｭｰｻｰちゃまの胸";
+window.im_cjs.t[7][2] = "に､そっと閉じ込めてください";
+window.im_cjs.t[7][3] = "まし｡";
+window.im_cjs.t[7][5] = "わたくしの気持ちは､";
+window.im_cjs.t[7][6] = "あなただけが召し上がって♪";
