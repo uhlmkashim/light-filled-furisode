fdc15218102dd22e278a60ae191840c2a184a686
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-09-03 20:26:45 +0900

    Update talk_k.js

--- js/cjs/episode/416_3/talk_k.js
+++ js/cjs/episode/416_3/talk_k.js
@@ -15,18 +15,18 @@ window.im_cjs.t[2][2] = "그림이나 글자를 올리는 문화가";
 window.im_cjs.t[2][3] = "있었다니. 저, 아직도 공부가";
 window.im_cjs.t[2][4] = "부족한 거네요.";
 
-window.im_cjs.t[3][1] = "무엇을 쓸까, 굉장히 고민도";
+window.im_cjs.t[3][1] = "무엇을 쓸까, 고민도 많이";
 window.im_cjs.t[3][2] = "했어요. 그래도, 모처럼인걸요.";
 window.im_cjs.t[3][3] = "여기는 역시, 스트레이트한 생각을";
 window.im_cjs.t[3][4] = "전하고 싶어서요.";
 
 window.im_cjs.t[4][1] = "사실은, 오므라이스도 만들어";
-window.im_cjs.t[4][2] = "드리고 싶었는데요…… 촬영에";
+window.im_cjs.t[4][2] = "드리고 싶었지만요…… 촬영에";
 window.im_cjs.t[4][3] = "사용하는 것이니, 프로인 분께";
 window.im_cjs.t[4][4] = "맡겼어요.";
 
 window.im_cjs.t[5][1] = "그 대신, 오므라이스의 마무리는";
-window.im_cjs.t[5][2] = "없이 해주셨어요.";
+window.im_cjs.t[5][2] = "하지 않고 해주셨어요.";
 window.im_cjs.t[5][3] = "다들, 고개를 갸웃하고 계셨어요.";
 window.im_cjs.t[5][4] = "";
 
@@ -39,4 +39,4 @@ window.im_cjs.t[7][1] = "그러니까, 프로듀서 쨔마의 가슴에,";
 window.im_cjs.t[7][2] = "살며시 가두어 주세요.";
 window.im_cjs.t[7][3] = "";
 window.im_cjs.t[7][5] = "제 마음은,";
-window.im_cjs.t[7][6] = "당신만이 드시고♪";
+window.im_cjs.t[7][6] = "당신만이 드시길♪";
