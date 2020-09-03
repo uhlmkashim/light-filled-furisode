082dc69538845aa5ffcd324cb5b7259c6928ace6
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-09-03 20:19:59 +0900

    Create talk_k.js

---
+++ js/cjs/episode/416_3/talk_k.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "자, 어떤가요? 능숙하게 쓸 수";
+window.im_cjs.t[1][2] = "있었어요. 완벽한 상태로 프로듀서";
+window.im_cjs.t[1][3] = "쨔마에게 내놓기 위해서,";
+window.im_cjs.t[1][4] = "몰래 연습한 거에요♪";
+
+window.im_cjs.t[2][1] = "처음에는 놀랐어요. 오므라이스에,";
+window.im_cjs.t[2][2] = "그림이나 글자를 올리는 문화가";
+window.im_cjs.t[2][3] = "있었다니. 저, 아직도 공부가";
+window.im_cjs.t[2][4] = "부족한 거네요.";
+
+window.im_cjs.t[3][1] = "무엇을 쓸까, 굉장히 고민도";
+window.im_cjs.t[3][2] = "했어요. 그래도, 모처럼인걸요.";
+window.im_cjs.t[3][3] = "여기는 역시, 스트레이트한 생각을";
+window.im_cjs.t[3][4] = "전하고 싶어서요.";
+
+window.im_cjs.t[4][1] = "사실은, 오므라이스도 만들어";
+window.im_cjs.t[4][2] = "드리고 싶었는데요…… 촬영에";
+window.im_cjs.t[4][3] = "사용하는 것이니, 프로인 분께";
+window.im_cjs.t[4][4] = "맡겼어요.";
+
+window.im_cjs.t[5][1] = "그 대신, 오므라이스의 마무리는";
+window.im_cjs.t[5][2] = "없이 해주셨어요.";
+window.im_cjs.t[5][3] = "다들, 고개를 갸웃하고 계셨어요.";
+window.im_cjs.t[5][4] = "";
+
+window.im_cjs.t[6][1] = "하지만, 이유는 비밀로 했어요.";
+window.im_cjs.t[6][2] = "그야, 프로듀서 쨔마께";
+window.im_cjs.t[6][3] = "메시지를 보내기 위해서라니……";
+window.im_cjs.t[6][4] = "모두에게는 말할 수 없어요.";
+
+window.im_cjs.t[7][1] = "그러니까, 프로듀서 쨔마의 가슴에,";
+window.im_cjs.t[7][2] = "살며시 가두어 주세요.";
+window.im_cjs.t[7][3] = "";
+window.im_cjs.t[7][5] = "제 마음은,";
+window.im_cjs.t[7][6] = "당신만이 드시고♪";
