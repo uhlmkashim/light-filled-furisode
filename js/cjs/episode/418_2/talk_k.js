4e7d7db8ff222e2496f2ede26b88fd166072ef05
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-09-30 16:47:47 +0900

    Create talk_k.js

---
+++ js/cjs/episode/418_2/talk_k.js
@@ -0,0 +1,52 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "와앗! 보세요 프로듀서";
+window.im_cjs.t[1][2] = "씨! 꽃이 잔뜩 늘어서";
+window.im_cjs.t[1][3] = "있어요! 이렇게 잔뜩……";
+window.im_cjs.t[1][4] = "어떤 것도 너무 예뻐……!";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[2][1] = "초록빛에 오렌지에 노란색…… 모두,";
+window.im_cjs.t[2][2] = "제 이미지컬러로 준비해";
+window.im_cjs.t[2][3] = "주셨네요! 앗, 제 일러스트까지";
+window.im_cjs.t[2][4] = "있어요! 멋져……!";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[3][1] = "프로듀서 씨, 한 장 찍어도";
+window.im_cjs.t[3][2] = "될까요? ……찍어 주시는";
+window.im_cjs.t[3][3] = "건가요? 감사합니다!";
+window.im_cjs.t[3][4] = "그럼, 부탁드립니다.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[4][1] = "……잠깐만, 죄송해요.";
+window.im_cjs.t[4][2] = "조금 눈물이 나버려서…….";
+window.im_cjs.t[4][3] = "기뻐서, 가슴이 벅차올랐어요.";
+window.im_cjs.t[4][4] = "에헤♪";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[5][1] = "꽃, 늘었네요. 그 때보다도";
+window.im_cjs.t[5][2] = "훨씬. 그만큼, 모두의 미소도";
+window.im_cjs.t[5][3] = "늘었다는 걸까?";
+window.im_cjs.t[5][4] = "……그런거면 좋겠다.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[6][1] = "기억 속의 저는, 미소짓지 않은";
+window.im_cjs.t[6][2] = "날도 있었어요. 하지만, 노력할 수 ";
+window.im_cjs.t[6][3] = "있었어요. 노력한 앞에,";
+window.im_cjs.t[6][4] = "미소가 있다고 믿었으니까요.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[7][1] = "저, 행복해요. 이렇게 사랑받을 수";
+window.im_cjs.t[7][2] = "있어서……. 앞으로도, 모두에게";
+window.im_cjs.t[7][3] = "미소를……";
+window.im_cjs.t[7][5] = "함께 보내요,";
+window.im_cjs.t[7][6] = "프로듀서 씨♪";
+
+
+
+
