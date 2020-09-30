9fc43e45240720eb2c82eb030cfa73b22c8f3da8
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-09-30 17:27:44 +0900

    Create talk_k.js

---
+++ js/cjs/episode/419_3/talk_k.js
@@ -0,0 +1,43 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "찾았다, 나쁜 아이! 나를 기다리게";
+window.im_cjs.t[1][2] = "하다니, 배짱이 두둑하잖아!";
+window.im_cjs.t[1][3] = "하지만 뭐, 오늘 밤 정도는,";
+window.im_cjs.t[1][4] = "특별히 용서해 줄게.";
+
+window.im_cjs.t[2][1] = "그야 오늘은, 즐겁고 즐거운";
+window.im_cjs.t[2][2] = "할로윈 파티인걸! 프로듀서에게";
+window.im_cjs.t[2][3] = "제멋대로 말해도, 아무리 장난을";
+window.im_cjs.t[2][4] = "쳐도, 용서받는 날!";
+
+window.im_cjs.t[3][1] = "그런 고로, 즉시 갈게!";
+window.im_cjs.t[3][2] = "당신, 각오와 준비는 되어 있는";
+window.im_cjs.t[3][3] = "거지? 뭐, ｢기다렸다｣라는";
+window.im_cjs.t[3][4] = "말은, 묻지 않겠지만♪";
+
+window.im_cjs.t[4][1] = "트릭 오아 트릿♪ 과자를 주지";
+window.im_cjs.t[4][2] = "않으면, 잔뜩 장난을 칠거야!";
+window.im_cjs.t[4][3] = "후흥, 마녀의 말은, 절대적이야?";
+window.im_cjs.t[4][4] = "자, 빨리빨리♪";
+
+window.im_cjs.t[5][1] = "와, 역시 준비했구나.";
+window.im_cjs.t[5][2] = "게다가, 내 취향의 맛있어 보이는";
+window.im_cjs.t[5][3] = "과자! 역시네, 잘 알고 있잖아♪";
+window.im_cjs.t[5][4] = "";
+
+window.im_cjs.t[6][1] = "앗! 벼, 별로, 칭찬한건 아냐!?";
+window.im_cjs.t[6][2] = "이 정도는, 당연한거야,";
+window.im_cjs.t[6][3] = "당연! 당신, 얼마나 내 곁에";
+window.im_cjs.t[6][4] = "있었다고 생각해!";
+
+window.im_cjs.t[7][1] = "그래도…… 그렇네, 좋아. 모처럼";
+window.im_cjs.t[7][2] = "이니까, 답례를 줄게! 당신이";
+window.im_cjs.t[7][3] = "기뻐할 것 같은……";
+
+window.im_cjs.t[7][5] = "소중히 간직해둔,";
+window.im_cjs.t[7][6] = "마녀의 트릭으로♪";
