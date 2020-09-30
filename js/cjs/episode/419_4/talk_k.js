72166d2dfdcdd78a262adef4a793ff4badeed8cd
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-09-30 16:12:06 +0900

    Create talk_k.js

---
+++ js/cjs/episode/419_4/talk_k.js
@@ -0,0 +1,52 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "……어머, 프로듀서. 들켜버렸네요.";
+window.im_cjs.t[1][2] = "제 가장은 아직 준비 중이에요.";
+window.im_cjs.t[1][3] = "트릭 오아 트릿에는,";
+window.im_cjs.t[1][4] = "아직 조금 이를지도.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[2][1] = "저는, 미라의 모습을 하려고";
+window.im_cjs.t[2][2] = "했는데요…… 보시다시피,";
+window.im_cjs.t[2][3] = "얽혀버리고 말았네요.";
+window.im_cjs.t[2][4] = "붕대가 붕괴되어…… 후후♪";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[3][1] = "아직 취하지는 않았어요?";
+window.im_cjs.t[3][2] = "할로윈 풍미의 칵테일을 마시는 건,";
+window.im_cjs.t[3][3] = "파티가 시작되고부터.";
+window.im_cjs.t[3][4] = "프로듀서와 건배하고 나서에요.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[4][1] = "처음엔, 어떻게든 하려고 바동바동";
+window.im_cjs.t[4][2] = "거렸는데요…… 한층 더";
+window.im_cjs.t[4][3] = "더 얽혀버려서, 느긋이";
+window.im_cjs.t[4][4] = "풀어보기로 했어요.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[5][1] = "파티까지는 시간도 있고,";
+window.im_cjs.t[5][2] = "프로듀서의 의외의 미소도 이끌어";
+window.im_cjs.t[5][3] = "낼 수 있었으니. 느긋이 해서";
+window.im_cjs.t[5][4] = "다행인 것 같네요.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[6][1] = "이 뒤는…… 프로듀서도,";
+window.im_cjs.t[6][2] = "거들어주시면 감사하겠어요.";
+window.im_cjs.t[6][3] = "제가 하면, 또 한층";
+window.im_cjs.t[6][4] = "얽혀버릴 것 같아서요.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[7][1] = "미라에 장난을 당하길 바라시다면";
+window.im_cjs.t[7][2] = "그 후에…… 아니면, 지금, 저에게";
+window.im_cjs.t[7][3] = "장난쳐 보실래요? 후후, 프로듀서는,";
+window.im_cjs.t[7][4] = "어느 쪽일까.";
+window.im_cjs.t[7][5] = "트릭 오아……?";
+
+
+
+
