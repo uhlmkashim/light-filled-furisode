da7be18ff1e3c9d0dade1c13021cf1d59c44c9d6
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-10-10 01:08:18 +0900

    Create talk_k.js

---
+++ js/cjs/episode/420_2/talk_k.js
@@ -0,0 +1,48 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "네~. 호죠 카렌이야. 후훗,";
+window.im_cjs.t[1][2] = "대단한 환성♪서프라이즈한 보람,";
+window.im_cjs.t[1][3] = "있었을까나. 다들 깜짝 놀라고";
+window.im_cjs.t[1][4] = "있지만, 기쁜 것 같고.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[2][1] = "오늘은 모두와 만나고 싶어서,";
+window.im_cjs.t[2][2] = "시사회에 잠깐 실례했어.";
+window.im_cjs.t[2][3] = "다들 나를 만나고 싶었어?";
+window.im_cjs.t[2][4] = "그럼, 마찬가지네♪";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[3][1] = "지금부터 볼 영화는, 벌써 알고";
+window.im_cjs.t[3][2] = "있겠지. 나의 다큐멘터리……";
+window.im_cjs.t[3][3] = "과거의 나도, 지금의 나도, 전부";
+window.im_cjs.t[3][4] = "드러내서 찍었어.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[4][1] = "즐거운 장면만 있는 건 아니지만,";
+window.im_cjs.t[4][2] = "모두들이라면, 받아들여 주지";
+window.im_cjs.t[4][3] = "않을까 생각해.";
+window.im_cjs.t[4][4] = "……아니, 믿고 있어.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[5][1] = "그야, 모두는 지금까지 나를";
+window.im_cjs.t[5][2] = "지지해줬고…… 나와 함께 지금도,";
+window.im_cjs.t[5][3] = "미래도 걷고 싶다고 생각해주는";
+window.im_cjs.t[5][4] = "사람들이니까.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[6][1] = "상영이 끝난 뒤에, 잠깐,";
+window.im_cjs.t[6][2] = "시간을 좀 내줄 수 있을까?";
+window.im_cjs.t[6][3] = "모두에게 조금 이야기를 했으면";
+window.im_cjs.t[6][4] = "좋겠어. 모두, 괜찮지?";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[7][1] = "꿈을 포기하지 못하고 발버둥쳐서……";
+window.im_cjs.t[7][2] = "겨우 지금까지 이어진 나의";
+window.im_cjs.t[7][3] = "얘기를.";
+window.im_cjs.t[7][5] = "모두에게 꿈을 주고 싶은,";
+window.im_cjs.t[7][6] = "호죠 카렌이라는 아이돌의 얘기를.";
