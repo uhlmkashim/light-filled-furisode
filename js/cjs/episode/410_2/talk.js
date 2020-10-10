195b67985eccdbf51a0616e53c11cefcbfb1b702
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-10-11 00:49:04 +0900

    Create talk.js

---
+++ js/cjs/episode/410_2/talk.js
@@ -0,0 +1,41 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "どうです､この華麗な髪さばき! ";
+window.im_cjs.t[1][2] = "見事なものでしょうっ♪あっと";
+window.im_cjs.t[1][3] = "いう間に､ｷﾗｷﾗﾊﾟｰﾃｨｰｽﾀｲﾙ･むつ";
+window.im_cjs.t[1][4] = "みちゃんの完成です!";
+
+window.im_cjs.t[2][1] = "自分の髪でいろいろ練習した成";
+window.im_cjs.t[2][2] = "果が､今ここにっ! いざという";
+window.im_cjs.t[2][3] = "時に役立つ､さいきっくﾊﾟﾜｰ! ";
+window.im_cjs.t[2][4] = "……ではないんですけど｡";
+
+window.im_cjs.t[3][1] = "ですが､ﾌﾟﾛﾃﾞｭｰｻｰ! 可愛いﾊﾟﾜｰ";
+window.im_cjs.t[3][2] = "の高まりを､ずしっずしっと感";
+window.im_cjs.t[3][3] = "じますよね? ええ､そうでしょ";
+window.im_cjs.t[3][4] = "う! つまり､ｻｲｷｯｸ成功です!";
+
+window.im_cjs.t[4][1] = "えっへへ～♪今回のお仕事､";
+window.im_cjs.t[4][2] = "すごく楽しいんです! 結婚式! ";
+window.im_cjs.t[4][3] = "披露宴! ﾄﾞﾚｽｱｯﾌﾟに､ﾒｲｸｱｯﾌﾟ! ";
+window.im_cjs.t[4][4] = "夢がむくむく膨らみます♪";
+
+window.im_cjs.t[5][1] = "花嫁役のﾓﾃﾞﾙさんも､綺麗だっ";
+window.im_cjs.t[5][2] = "たなぁ～……｡今日のﾕｯｺは披露";
+window.im_cjs.t[5][3] = "宴のｹﾞｽﾄ役ですが､いつかの時";
+window.im_cjs.t[5][4] = "のため､勉強しようかと!";
+
+window.im_cjs.t[6][1] = "そして､私が花嫁さんになった";
+window.im_cjs.t[6][2] = "時には……ふふっ♪その時は､";
+window.im_cjs.t[6][3] = "もちろんﾌﾟﾛﾃﾞｭｰｻｰも一緒です! ";
+window.im_cjs.t[6][4] = "絶対絶対､一緒ですから! ";
+
+window.im_cjs.t[7][1] = "どんな花嫁になれるかな～♪";
+window.im_cjs.t[7][2] = "ふわふわﾄﾞﾚｽ､それともﾏｰﾒｲﾄﾞ? ";
+window.im_cjs.t[7][3] = "和装も可愛いかも! ﾌﾟﾛﾃﾞｭｰｻｰ､";
+window.im_cjs.t[7][4] = "本番が楽しみですねっ! ";
