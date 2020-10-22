6fca042b6905c574d89167bc2b51aecf8cc37a7c
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-10-22 23:40:27 +0900

    Create talk_k.js

---
+++ js/cjs/episode/421_2/talk_k.js
@@ -0,0 +1,48 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "한번 눌러서, 마무리 완료~! ";
+window.im_cjs.t[1][2] = "악수회, 언제든지 시작할 수 있어. ";
+window.im_cjs.t[1][3] = "차라리, 내가 들뜬 사이에 ";
+window.im_cjs.t[1][4] = "시작해버려서~.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[2][1] = "이거? 네 추측대로의 물건이야. ";
+window.im_cjs.t[2][2] = "내 기술과 변덕, ";
+window.im_cjs.t[2][3] = "그리고 너의 어드바이스로 ";
+window.im_cjs.t[2][4] = "완성된, 시키쨩의 자신작!";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[3][1] = "손목에 뿌리면, 악수할 때 ";
+window.im_cjs.t[3][2] = "향기가 확 풍기잖아? 그럼, ";
+window.im_cjs.t[3][3] = "그 아이는 이제 내 생각밖에 ";
+window.im_cjs.t[3][4] = "할 수 없게 되버리는거야♪";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[4][1] = "나, 이렇게 보여도 팬들이 ";
+window.im_cjs.t[4][2] = "마음에 든다구? 그들의 ";
+window.im_cjs.t[4][3] = "시선도, 냄새도, 가변적인 열도, ";
+window.im_cjs.t[4][4] = "자극을 주니까.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[5][1] = "그래서……더욱 더 실험하고 ";
+window.im_cjs.t[5][2] = "싶어졌어♪ 오늘은 내 향기를 ";
+window.im_cjs.t[5][3] = "투여하고, 반응을 관찰할거야! ";
+window.im_cjs.t[5][4] = "냐하하, 기대된다~.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[6][1] = "자, 그럼 즉시 실험을……";
+window.im_cjs.t[6][2] = "이 아니라, 악수회에 가자-♪";
+window.im_cjs.t[6][3] = "……그 전에, 자자, ";
+window.im_cjs.t[6][4] = "손 내밀어.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[7][1] = "왜냐니……악수하는거야. ";
+window.im_cjs.t[7][2] = "이 향기를 처음 맡을 권리는 ";
+window.im_cjs.t[7][3] = "너에게 있으니까.";
+window.im_cjs.t[7][5] = "그야 너는, 나의 팬, ";
+window.im_cjs.t[7][6] = "제1호잖아♪";
