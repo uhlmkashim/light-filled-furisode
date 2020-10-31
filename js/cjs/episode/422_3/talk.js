27dc06b928ebb57357e51ab981cf195ec0a61f70
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-10-31 19:50:52 +0900

    Create talk.js

---
+++ js/cjs/episode/422_3/talk.js
@@ -0,0 +1,44 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "はぁっ､気持ちいい……♪";
+window.im_cjs.t[1][2] = "心の奥底まで､じんわりと染み";
+window.im_cjs.t[1][3] = "わたっていく温かさ……それに､";
+window.im_cjs.t[1][4] = "紅葉がとっても綺麗……♪";
+
+window.im_cjs.t[2][1] = "美しく染まって､ちょうど盛りの";
+window.im_cjs.t[2][2] = "ときですね｡とめどなく天から降";
+window.im_cjs.t[2][3] = "り注いで……まるで夕陽の欠片";
+window.im_cjs.t[2][4] = "が落ちてくるよう｡";
+
+window.im_cjs.t[3][1] = "秋が散り始めれば､冬の予感……";
+window.im_cjs.t[3][2] = "気づいてしまえば､あっという間";
+window.im_cjs.t[3][3] = "に､雪が舞うほどの寒さがやって";
+window.im_cjs.t[3][4] = "くるんでしょうね｡";
+
+window.im_cjs.t[4][1] = "……今頃､ﾌﾟﾛﾃﾞｭｰｻｰさんも､同じ";
+window.im_cjs.t[4][2] = "景色を見ているのかな｡……移ろ";
+window.im_cjs.t[4][3] = "いゆく秋の空に､何を想うんで";
+window.im_cjs.t[4][4] = "しょうか｡";
+
+window.im_cjs.t[5][1] = "同じ木から落ちた葉が､一枚";
+window.im_cjs.t[5][2] = "一枚､異なる色を持つように……";
+window.im_cjs.t[5][3] = "私たちもまた､同じ景色から様々";
+window.im_cjs.t[5][4] = "な想いを抱くもの……｡";
+
+window.im_cjs.t[6][1] = "それを確かめ合うために､私たち";
+window.im_cjs.t[6][2] = "は語り合うのですね｡私も知りた";
+window.im_cjs.t[6][3] = "いですから｡あなたの心に映る､";
+window.im_cjs.t[6][4] = "美しい秋の風景を……｡";
+
+window.im_cjs.t[7][1] = "時はまだ､夕暮れどき｡秋の夜長";
+window.im_cjs.t[7][2] = "と言いますし……";
+window.im_cjs.t[7][5] = "今夜はﾌﾟﾛﾃﾞｭｰｻｰさんと､";
+window.im_cjs.t[7][6] = "たくさんのお話ができたら……♪";
+
+
+
