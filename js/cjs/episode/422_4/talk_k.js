d713f00b42e03e516c6a211efad44ac329a16bd5
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-11-08 12:34:46 +0900

    Create talk_k.js

---
+++ js/cjs/episode/422_4/talk_k.js
@@ -0,0 +1,43 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "슥삭슥삭 슥삭슥삭, 거품망에";
+window.im_cjs.t[1][2] = "거품을 잔뜩 내서…… 큰 덩어리";
+window.im_cjs.t[1][3] = "하나로 뭉쳐서……쫄깃한 반죽이";
+window.im_cjs.t[1][4] = "완성♪";
+
+window.im_cjs.t[2][1] = "다음은, 통을 뒤집어서 접시로";
+window.im_cjs.t[2][2] = "만들어! 그리고나서 나뭇잎과 나무";
+window.im_cjs.t[2][3] = "열매를 흩뿌리고, 거품을 올리고,";
+window.im_cjs.t[2][4] = "동그랗게, 동그랗게……";
+
+window.im_cjs.t[3][1] = "짜쟌-! 거품으로 만든,";
+window.im_cjs.t[3][2] = "새하얀 도넛! 음, 귀여워!";
+window.im_cjs.t[3][3] = "거기다 뭣보다도 맛있어보여!";
+window.im_cjs.t[3][4] = "나, 센스가 있을지도!";
+
+window.im_cjs.t[4][1] = "온천 한정의 스페셜 맛, 프로듀서";
+window.im_cjs.t[4][2] = "에게도 보여주고싶다~! 으음,";
+window.im_cjs.t[4][3] = "기왕이면, 함께 만들었으면";
+window.im_cjs.t[4][4] = "좋았을텐데.";
+
+window.im_cjs.t[5][1] = "……프로듀서, 아직 일 끝나지";
+window.im_cjs.t[5][2] = "않은 걸까? 모처럼 온천에";
+window.im_cjs.t[5][3] = "와 있는 건데, 여유로우면";
+window.im_cjs.t[5][4] = "좋겠네~…….";
+
+window.im_cjs.t[6][1] = "좋아! 온천에서 나가면,";
+window.im_cjs.t[6][2] = "돌격해버리자! 모처럼";
+window.im_cjs.t[6][3] = "온천인걸, 방에 틀어박혀";
+window.im_cjs.t[6][4] = "있으면 아깝다구!";
+
+window.im_cjs.t[7][1] = "지역 한정인 맛있는 도넛이라던가,";
+window.im_cjs.t[7][2] = "있을지도 모르고! 프로듀서랑";
+window.im_cjs.t[7][3] = "즐거운 추억,";
+
+window.im_cjs.t[7][5] = "프로듀서랑 즐거운 추억,";
+window.im_cjs.t[7][6] = "잔뜩 만들지않으면 안되니까♪";
