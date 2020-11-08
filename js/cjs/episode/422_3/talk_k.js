bc15ac718cdc9cf810ed992fd5a2bfa6420126d1
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-11-08 11:41:59 +0900

    Create talk_k.js

---
+++ js/cjs/episode/422_3/talk_k.js
@@ -0,0 +1,44 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "하앗, 기분 좋아……♪";
+window.im_cjs.t[1][2] = "마음 속까지, 천천히 스며드는";
+window.im_cjs.t[1][3] = "따뜻함…… 거기다,";
+window.im_cjs.t[1][4] = "단풍이 정말로 예뻐……♪";
+
+window.im_cjs.t[2][1] = "아름답게 물들어서, 막 한창";
+window.im_cjs.t[2][2] = "때군요. 끝없이 하늘에서";
+window.im_cjs.t[2][3] = "떨어져 내리고…… 정말로 노을의";
+window.im_cjs.t[2][4] = "조각이 내리는 것 같아.";
+
+window.im_cjs.t[3][1] = "가을이 시들 무렵엔, 겨울의 예감……";
+window.im_cjs.t[3][2] = "눈치채버리면, 어느샌가,";
+window.im_cjs.t[3][3] = "눈이 흩날릴 정도의 추위가";
+window.im_cjs.t[3][4] = "와버리죠.";
+
+window.im_cjs.t[4][1] = "……지금 쯤, 프로듀서씨도, 같은";
+window.im_cjs.t[4][2] = "경치를 보고계신걸까. ……";
+window.im_cjs.t[4][3] = "변해가는 가을 하늘에, 무얼";
+window.im_cjs.t[4][4] = "생각하고 계신걸까.";
+
+window.im_cjs.t[5][1] = "같은 나무에서 떨어진 잎이, 한장";
+window.im_cjs.t[5][2] = "한장, 다른 색을 가지는 것처럼……";
+window.im_cjs.t[5][3] = "우리들도 또한, 같은 경치에서";
+window.im_cjs.t[5][4] = "다양한 감정을 품는 것…….";
+
+window.im_cjs.t[6][1] = "그것을 서로 확인하기 위해, 우리가";
+window.im_cjs.t[6][2] = "이야기를 나누는 거네요. 나도";
+window.im_cjs.t[6][3] = "알고 싶으니까요. 당신 마음에 비친,";
+window.im_cjs.t[6][4] = "아름다운 가을 풍경을…….";
+
+window.im_cjs.t[7][1] = "시간은 아직, 해질녘이라. 가을은";
+window.im_cjs.t[7][2] = "긴긴밤이라고도 하고……";
+window.im_cjs.t[7][5] = "오늘 밤은 프로듀서씨와,";
+window.im_cjs.t[7][6] = "잔뜩 얘기할 수 있었으면……♪";
+
+
+
