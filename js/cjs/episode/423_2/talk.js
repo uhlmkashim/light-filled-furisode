bc00032a98e4bc9e46e32bc9a65e5003da5431b7
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-11-16 19:37:46 +0900

    Create talk.js

---
+++ js/cjs/episode/423_2/talk.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "そろそろ焼けてきたかしら｡";
+window.im_cjs.t[1][2] = "それじゃあ､ﾐｽﾞｷの返しﾃｸﾆｯｸを";
+window.im_cjs.t[1][3] = "ご覧あれ! くるくるーっと……";
+window.im_cjs.t[1][4] = "よしっ､完璧だわ♪";
+
+window.im_cjs.t[2][1] = "これでも大阪の女だもの｡";
+window.im_cjs.t[2][2] = "家にたこ焼き器はあるし､完璧に";
+window.im_cjs.t[2][3] = "焼けるわ! 舐めたらあかんっ!";
+window.im_cjs.t[2][4] = "なんて､びっくりしたかしら?";
+
+window.im_cjs.t[3][1] = "ｱﾅｳﾝｻｰを目指してた頃､標準語で";
+window.im_cjs.t[3][2] = "話すように矯正したんだけどね｡";
+window.im_cjs.t[3][3] = "実家に帰ったり､気を抜いたりす";
+window.im_cjs.t[3][4] = "ると､出るわねぇ｡";
+
+window.im_cjs.t[4][1] = "あとは……そうね｡事務所のみん";
+window.im_cjs.t[4][2] = "なで呑むときも､つい出ちゃった";
+window.im_cjs.t[4][3] = "りするわ｡たまにﾘｸｴｽﾄされるこ";
+window.im_cjs.t[4][4] = "ともあるけど♪";
+
+window.im_cjs.t[5][1] = "関西弁で､可愛く言ってみて～っ";
+window.im_cjs.t[5][2] = "てやつよ♪あら､ﾌﾟﾛﾃﾞｭｰｻｰ君も";
+window.im_cjs.t[5][3] = "何か聞きたい? そうねぇ､よく";
+window.im_cjs.t[5][4] = "ﾘｸｴｽﾄされる言葉は……｡";
+
+window.im_cjs.t[6][1] = "めっちゃ好きやねん! とか……";
+window.im_cjs.t[6][2] = "あ､あら､別に他意はないわよ?";
+window.im_cjs.t[6][3] = "でも､あなたが相手だと､ちょっ";
+window.im_cjs.t[6][4] = "ぴり照れるわ……ふぅー｡";
+
+window.im_cjs.t[7][1] = "あっ､ほら､焼けたわよ!";
+window.im_cjs.t[7][2] = "熱いうちに食べましょ｡ずっと";
+window.im_cjs.t[7][3] = "焼いてるからかしら｡";
+window.im_cjs.t[7][5] = "顔まで熱く";
+window.im_cjs.t[7][6] = "なってきちゃった……♪";
