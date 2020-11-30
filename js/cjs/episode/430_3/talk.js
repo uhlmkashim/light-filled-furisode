8562b79a249d65b7c49757328fc05b2921b5b69f
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2020-12-01 00:08:26 +0900

    Create talk.js

---
+++ js/cjs/episode/430_3/talk.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "わぁ､ｸﾘｽﾏｽｴﾝｼﾞｪﾙ……♪私たち";
+window.im_cjs.t[1][2] = "の元にも､幸せを運んできてく";
+window.im_cjs.t[1][3] = "れたんでしょうか? もしそう";
+window.im_cjs.t[1][4] = "だったら､嬉しいな……♪";
+
+window.im_cjs.t[2][1] = "私､今日が来るのを､心待ちにし";
+window.im_cjs.t[2][2] = "てて｡ﾌﾟﾛﾃﾞｭｰｻｰさんが声をかけ";
+window.im_cjs.t[2][3] = "てくれたときから､ずーっとﾄﾞｷ";
+window.im_cjs.t[2][4] = "ﾄﾞｷﾜｸﾜｸしてたんですよ｡";
+
+window.im_cjs.t[3][1] = "みんなと過ごすｸﾘｽﾏｽ……何を";
+window.im_cjs.t[3][2] = "しようかな､どんなことを話そ";
+window.im_cjs.t[3][3] = "うかなって､毎晩考えて､夢の中";
+window.im_cjs.t[3][4] = "で何度もﾊﾟｰﾃｨｰをしました｡";
+
+window.im_cjs.t[4][1] = "ふふっ……おかしいですよね｡";
+window.im_cjs.t[4][2] = "でもきっと､今夜のﾊﾟｰﾃｨｰは､";
+window.im_cjs.t[4][3] = "夢よりも素敵で､忘れられない";
+window.im_cjs.t[4][4] = "思い出になるんだろうなぁ｡";
+
+window.im_cjs.t[5][1] = "だって……準備をしてる今から､";
+window.im_cjs.t[5][2] = "笑顔があふれてますから｡";
+window.im_cjs.t[5][3] = "楽しくて､嬉しくて……お喋り";
+window.im_cjs.t[5][4] = "だってとまりません♪";
+
+window.im_cjs.t[6][1] = "えへへ､大丈夫ですよ､手もちゃ";
+window.im_cjs.t[6][2] = "んと動かしてますから｡飾りつけ";
+window.im_cjs.t[6][3] = "だって､この子を飾ったら……";
+window.im_cjs.t[6][4] = "あとは､お星様を飾るだけ｡";
+
+window.im_cjs.t[7][1] = "だから……ゆっくり､";
+window.im_cjs.t[7][2] = "丁寧にやらせてください｡";
+window.im_cjs.t[7][3] = "ﾌﾟﾛﾃﾞｭｰｻｰさんと過ごすｸﾘｽﾏｽに､";
+window.im_cjs.t[7][5] = "もう少しだけ､";
+window.im_cjs.t[7][6] = "浸っていたいから……♪";
