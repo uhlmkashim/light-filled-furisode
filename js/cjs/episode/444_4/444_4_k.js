4458da9cafb053a743de771cdbc58c58044d5bd9
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2021-05-31 19:47:08 +0900

    Update 444_4_k.js

--- js/cjs/episode/444_4/444_4_k.js
+++ js/cjs/episode/444_4/444_4_k.js
@@ -3847,7 +3847,7 @@ p.nominalBounds = new cjs.Rectangle(-97.4,0,92.4,18);
 	}
 	this.frame_166 = function() {
 		exportRoot._g.m1 = this.m.t[exportRoot._g.idx][1];
-		exportRoot._g.m2 = "……";
+		exportRoot._g.m2 = "";
 		exportRoot._u.setText();
 		exportRoot._u.setFace("ch1", 8);
 	}
