98b72e105fc130e3136666a62e2861ae044c64a6
uhlmkashim <37768917+uhlmkashim@users.noreply.github.com>
2021-05-31 19:32:51 +0900

    Create talk_k.js

---
+++ js/cjs/episode/444_4/talk_k.js
@@ -0,0 +1,50 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "꺗, 계단에 발끝이";
+window.im_cjs.t[1][2] = "걸려버렸어요~…… 라니,";
+window.im_cjs.t[1][3] = "와왓! 너, 넘어진다~!";
+window.im_cjs.t[1][4] = "비, 비켜주세요~!";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[2][1] = "(아우~…… 바보같았어요";
+window.im_cjs.t[2][2] = "…… 드레스가 너무 귀엽고,";
+window.im_cjs.t[2][3] = "기뻐서, 들떠있다보니, 이런";
+window.im_cjs.t[2][4] = "일이~!)";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[3][1] = "(어, 어떡해~! ……그래도,";
+window.im_cjs.t[3][2] = "그다지 높지 않으니까, 다치진";
+window.im_cjs.t[3][3] = "않으려나…… 그래도, 귀여운 드레스로";
+window.im_cjs.t[3][4] = "넘어지고 싶진 않네……)";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[4][1] = "(라니, 프로듀서 씨!? 비켜주지";
+window.im_cjs.t[4][2] = "않으시는 건가요~!? 음";
+window.im_cjs.t[4][3] = "…… 혹시, 받아주실";
+window.im_cjs.t[4][4] = "생각인 걸까……?)";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[5][1] = "(촬영을 위해, 과자는 조금";
+window.im_cjs.t[5][2] = "삼가고 있었지만…… 우으,";
+window.im_cjs.t[5][3] = "안겼다가, 혹시 무겁다고";
+window.im_cjs.t[5][4] = "생각되면……)";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[6][1] = "(……아냐. 프로듀서 씨는,";
+window.im_cjs.t[6][2] = "언제라도 나를 생각해 주시고,";
+window.im_cjs.t[6][3] = "어떤 나라도 받아주셔……";
+window.im_cjs.t[6][4] = "그러니까, 괜찮아!)";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[7][1] = "프로듀서 씨, 부탁드려요!";
+window.im_cjs.t[7][2] = "저를 제대로 받아내서……";
+window.im_cjs.t[7][3] = "";
+
+window.im_cjs.t[7][5] = "부디, 그대로 놓지";
+window.im_cjs.t[7][6] = "말아주세요!";
+
