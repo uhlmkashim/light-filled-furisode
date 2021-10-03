491d6d0a02c4da88117492559e622058efdb9e54
uhlmkashim <sagisawa0330@gmail.com>
2021-10-03 21:56:49 +0900

    1

---
+++ js/cjs/episode/454_3/talk_k.js
@@ -0,0 +1,48 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "어서옵쇼~! ……라니";
+window.im_cjs.t[1][2] = "우와아아아!? 프로듀서 씨!?";
+window.im_cjs.t[1][3] = "아, 야키소바 하나, 매번~ ……";
+window.im_cjs.t[1][4] = "가 아니라 왜 있는 거야!?";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[2][1] = "일부러 와 준 거냐구!";
+window.im_cjs.t[2][2] = "언제나 바쁜 것 같은데……";
+window.im_cjs.t[2][3] = "그래도…… 고마워.";
+window.im_cjs.t[2][4] = "헤헷, 당신은 성실하구나♪";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[3][1] = "어때? 우리 가게, 꽤 번창하고";
+window.im_cjs.t[3][2] = "있지? 역시 문화제라고";
+window.im_cjs.t[3][3] = "하면 야키소바구나~!";
+window.im_cjs.t[3][4] = "게다가, 맛도 평가가 좋아!";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[4][1] = "모처럼이니까, 먹어 먹어!";
+window.im_cjs.t[4][2] = "서비스로 곱빼기로 해줄게! 어제는";
+window.im_cjs.t[4][3] = "린과 카렌도 와서, 맛있다고";
+window.im_cjs.t[4][4] = "보증해줬어!";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[5][1] = "……그러고보니, 프로듀서 씨도";
+window.im_cjs.t[5][2] = "오늘은 오프였나? 벌써 다른";
+window.im_cjs.t[5][3] = "반이라든가 봤어? 우리 학교,";
+window.im_cjs.t[5][4] = "훌륭하게 기합들어 있지!";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[6][1] = "추천은 역시 연극부와 경음부일까.";
+window.im_cjs.t[6][2] = "다들 능숙하다구.";
+window.im_cjs.t[6][3] = "……나는 이제 곧 교대니까,";
+window.im_cjs.t[6][4] = "괜찮다면 함께…… 갈래?";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[7][1] = "……아. 혹시 처음부터";
+window.im_cjs.t[7][2] = "그럴 생각으로 이 시간에 왔어?";
+window.im_cjs.t[7][3] = "뭐야~! 정말~!!";
+window.im_cjs.t[7][4] = "……이제 조금이니까,";
+window.im_cjs.t[7][5] = "기다려!";
