4a1afe44d7b357233259ab468f5f2e95d997e692
uhlmkashim <sagisawa0330@gmail.com>
2021-10-07 04:48:16 +0900

    1

---
+++ js/cjs/episode/431_2/talk_k.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "에헤헷, 귀엽게 됐을까?";
+window.im_cjs.t[1][2] = "제가 엄청 좋아하는 크리스마스를,";
+window.im_cjs.t[1][3] = "가득 담아 봤어요! 기뻐해준다면,";
+window.im_cjs.t[1][4] = "되게 좋겠다♪";
+
+window.im_cjs.t[2][1] = "메시지는, 크리스마스 축하와,";
+window.im_cjs.t[2][2] = "언제나 사이좋게 대해주시는 모두에";
+window.im_cjs.t[2][3] = "대한 감사의 마음입니다. 다음에,";
+window.im_cjs.t[2][4] = "또 놀았으면하고!";
+
+window.im_cjs.t[3][1] = "받으신 분들이, 어떤 표정을";
+window.im_cjs.t[3][2] = "지으실까? 기뻐해주실까?";
+window.im_cjs.t[3][3] = "생각하면 가슴이 따끈따끈해져요♪";
+window.im_cjs.t[3][4] = "";
+
+window.im_cjs.t[4][1] = "모두의 크리스마스를 꾸며줄";
+window.im_cjs.t[4][2] = "아이템이 됐으면 좋겠다♪ 에헤헷,";
+window.im_cjs.t[4][3] = "프로듀서 씨가 도와주셨고,";
+window.im_cjs.t[4][4] = "자신 있어요!";
+
+window.im_cjs.t[5][1] = "오늘은 어울려주셔서,";
+window.im_cjs.t[5][2] = "감사합니다!";
+window.im_cjs.t[5][3] = "귀여운 맞춤 키홀더도 살 수";
+window.im_cjs.t[5][4] = "있어서, 너~무 기뻐요!";
+
+window.im_cjs.t[6][1] = "맞춤이라니, 멋지네요.";
+window.im_cjs.t[6][2] = "곁에 없더라도, 둘이 이어져 있는";
+window.im_cjs.t[6][3] = "기분이라. 바라보는 것만으로도,";
+window.im_cjs.t[6][4] = "행복해♪";
+
+window.im_cjs.t[7][1] = "기쁠 때, 즐거울 때, 그리고……";
+window.im_cjs.t[7][2] = "조금 외로울 때.";
+window.im_cjs.t[7][3] = "그 키홀더를 보며,";
+window.im_cjs.t[7][5] = "프로듀서 씨의 미소를";
+window.im_cjs.t[7][6] = "떠올리네요♪";
