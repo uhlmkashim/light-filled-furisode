90e7d3eb1bf2cf6d63de1cb24d4d266e5e7eed38
uhlmkashim <sagisawa0330@gmail.com>
2021-10-07 05:50:21 +0900

    1

---
+++ js/cjs/episode/424_2/talk_k.js
@@ -0,0 +1,49 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "아아…… 다행이다, 발견했어.";
+window.im_cjs.t[1][2] = "후훗…… 나답지 않게,";
+window.im_cjs.t[1][3] = "찾아다녔어. 너무나도";
+window.im_cjs.t[1][4] = "프로듀서 씨의 얼굴이 보고 싶어서.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[2][1] = "LIVE가 최고로 고조돼서,";
+window.im_cjs.t[2][2] = "달성감도 만족감도 있지만";
+window.im_cjs.t[2][3] = "…… 아직 어딘가, 끝났단 게";
+window.im_cjs.t[2][4] = "믿기지가 않아서.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[3][1] = "……어쩌면, 믿고 싶지 않은";
+window.im_cjs.t[3][2] = "걸지도. 계~속 이 순간에";
+window.im_cjs.t[3][3] = "잠겨 있고 싶어…… 그렇게 생각될";
+window.im_cjs.t[3][4] = "만큼, 좋은 LIVE였으니까.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[4][1] = "팬들에게도, 특별한 추억을";
+window.im_cjs.t[4][2] = "남겼으면 좋겠다. 떠올렸을 때,";
+window.im_cjs.t[4][3] = "살짝 미소가 흘러나오는 기억이";
+window.im_cjs.t[4][4] = "된다면, 멋지겠네.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[5][1] = "원한다면, 프로듀서 씨의 마음에도.";
+window.im_cjs.t[5][2] = "오늘의 추억과, 나의 모습이,";
+window.im_cjs.t[5][3] = "쭉 선명한 그대로,";
+window.im_cjs.t[5][4] = "계속 빛나길 바라.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[6][1] = "내 가슴엔 말야, 그렇게";
+window.im_cjs.t[6][2] = "프로듀서 씨와 자아내온 매일이,";
+window.im_cjs.t[6][3] = "가득 담겨있어. 반짝반짝 빛나는,";
+window.im_cjs.t[6][4] = "나만의 보물이야.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[7][1] = "여기까지 나와 걸어와줘서, 고마워.";
+window.im_cjs.t[7][2] = "앞으로도, 당신의 손으로 빛내줘.";
+window.im_cjs.t[7][3] = "";
+window.im_cjs.t[7][5] = "나는, 당신이 키운";
+window.im_cjs.t[7][6] = "아이돌이니까.";
+
