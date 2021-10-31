26a47c1fed76d2b0e07fc368ba668a9b0343ca9a
uhlmkashim <sagisawa0330@gmail.com>
2021-10-31 23:20:03 +0900

    1

---
+++ js/cjs/episode/44/2_4/talk_k.js
@@ -0,0 +1,48 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "확실히 지켜보고…… 지금이닷!";
+window.im_cjs.t[1][2] = "앗! 프로듀서 씨, 맞았어요!";
+window.im_cjs.t[1][3] = "굉장해~!";
+window.im_cjs.t[1][4] = "히트예요 히트! 해냈어!";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[2][1] = "도착했을 땐, 솔직히……";
+window.im_cjs.t[2][2] = "능숙하게 하지 못하면 어쩌나";
+window.im_cjs.t[2][3] = "생각했었지만요……";
+window.im_cjs.t[2][4] = "그런거 전혀, 상관 없었네요!";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[3][1] = "칠 수 없어도 즐겁고,";
+window.im_cjs.t[3][2] = "칠 수 있으면 더 즐거워! 볼을";
+window.im_cjs.t[3][3] = "기다릴 때마다 두근두근하고,";
+window.im_cjs.t[3][4] = "헛스윙할때마다, 불타올라요!";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[4][1] = "배팅이란…… 아니,";
+window.im_cjs.t[4][2] = "스포츠란, 제가 제대로 마주하면,";
+window.im_cjs.t[4][3] = "생각했던 것보다 더욱더,";
+window.im_cjs.t[4][4] = "두근두근한 거네요.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[5][1] = "처음엔 그저, 운동부족을";
+window.im_cjs.t[5][2] = "해소하는 것만을 생각했어요.";
+window.im_cjs.t[5][3] = "하지만, 여기서 잔뜩 땀을 흘리고";
+window.im_cjs.t[5][4] = "나니, 기분도 달라져서……";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[6][1] = "저, 깨달았어요. 몸을 움직이는 건";
+window.im_cjs.t[6][2] = "즐겁고, 무척 기분이 좋아진다는 걸!";
+window.im_cjs.t[6][3] = "마음까지 맑아진 것 같아요!";
+window.im_cjs.t[6][4] = "";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[7][1] = "좋~아! 친절하게 가르쳐주신";
+window.im_cjs.t[7][2] = "성과를 보여드리기 위해서……";
+window.im_cjs.t[7][3] = "다음에 노리는 건,";
+window.im_cjs.t[7][5] = "프로듀서 씨께 바치는";
+window.im_cjs.t[7][6] = "특대 홈런이예요♪";
