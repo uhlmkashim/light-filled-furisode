35c4a1a07c216322027e906a17ff421b825e127f
uhlmkashim <sagisawa0330@gmail.com>
2021-11-01 20:55:16 +0900

    1

---
+++ js/cjs/episode/44/3_2/talk_k.js
@@ -0,0 +1,48 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "프로듀서…… 이쪽……. 옆에,";
+window.im_cjs.t[1][2] = "앉아……? 여기…… 내가";
+window.im_cjs.t[1][3] = "좋아하는 장소……. 프로듀서랑";
+window.im_cjs.t[1][4] = "…… 오고 싶었어…….";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[2][1] = "아빠랑 엄마는…… 일…… 아직";
+window.im_cjs.t[2][2] = "남았다고……. 조금……";
+window.im_cjs.t[2][3] = "둘이서 놀라고…….";
+window.im_cjs.t[2][4] = "그래서…… 여기에 왔어…….";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[3][1] = "초록빛이…… 예뻐……. 물도……";
+window.im_cjs.t[3][2] = "시원해…… 기분 좋아…….";
+window.im_cjs.t[3][3] = "앉아 있기만 해도…… 프로듀서도";
+window.im_cjs.t[3][4] = "…… 기운 날 수 있어…….";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[4][1] = "그치만…… 오늘은…… 그뿐만이";
+window.im_cjs.t[4][2] = "아냐……. 심심……";
+window.im_cjs.t[4][3] = "하지 않게…… 비누방울도……";
+window.im_cjs.t[4][4] = "가져 왔어…….";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[5][1] = "전에…… 사무소 사람들과……";
+window.im_cjs.t[5][2] = "밖에서 불었어……. 즐거웠어";
+window.im_cjs.t[5][3] = "……. 프로듀서랑도……";
+window.im_cjs.t[5][4] = "하고 싶다고…… 생각했어…….";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[6][1] = "여기서 불면…… 자,";
+window.im_cjs.t[6][2] = "봐봐……. 햇빛으로……";
+window.im_cjs.t[6][3] = "반짝반짝거려……. 후훗……";
+window.im_cjs.t[6][4] = "생각했던 대로…… 예뻐…….";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[7][1] = "프로듀서도…… 불자……?";
+window.im_cjs.t[7][2] = "내 거…… 빌려줄게……. 시간은……";
+window.im_cjs.t[7][3] = "있으니까…….";
+window.im_cjs.t[7][4] = "함께…… 한가롭게……";
+window.im_cjs.t[7][5] = "느긋이…… 하자.";
