25b8cf1cb738dac46245b1fb56abd76642157ba0
uhlmkashim <sagisawa0330@gmail.com>
2021-11-01 20:33:26 +0900

    1

---
+++ js/cjs/episode/45/2_2/talk_k.js
@@ -0,0 +1,48 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "프로듀서, 이거, 받아~…….";
+window.im_cjs.t[1][2] = "붉고…… 반짝반짝거리는";
+window.im_cjs.t[1][3] = "나무열매랑~…… 핑크색,";
+window.im_cjs.t[1][4] = "둥실둥실한 꽃이야~…….";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[2][1] = "다같이…… 가져왔어~…….";
+window.im_cjs.t[2][2] = "프로듀서에게, 차입~…….";
+window.im_cjs.t[2][3] = "코즈에는, 핑크색 꽃……";
+window.im_cjs.t[2][4] = "찾았어~…….";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[3][1] = "그리고~…… 레슨하고~……";
+window.im_cjs.t[3][2] = "노래하고~…… 후와아…….";
+window.im_cjs.t[3][3] = "음~…… 코즈에~……";
+window.im_cjs.t[3][4] = "아직, 낮잠 안 잤어~……?";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[4][1] = "일이 끝날 때까지……";
+window.im_cjs.t[4][2] = "프로듀서, 보고 있어~…….";
+window.im_cjs.t[4][3] = "코즈에의 응원……";
+window.im_cjs.t[4][4] = "필요해~? 알았어~……?";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[5][1] = "프로듀서, 힘내~…….";
+window.im_cjs.t[5][2] = "음~…… 이상하네~……";
+window.im_cjs.t[5][3] = "벌써, 힘껏 힘내고 있어~……?";
+window.im_cjs.t[5][4] = "";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[6][1] = "응원이 아니라~……";
+window.im_cjs.t[6][2] = "잘했어 잘했어의, 참잘했어요~…….";
+window.im_cjs.t[6][3] = "포상, 잔뜩 받고 싶구나~……?";
+window.im_cjs.t[6][4] = "";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[7][1] = "꽃이랑~…… 열매랑~……";
+window.im_cjs.t[7][2] = "코즈에~…….";
+window.im_cjs.t[7][3] = "이거 말고도 포상…… 필요해~?";
+window.im_cjs.t[7][4] = "코즈에,";
+window.im_cjs.t[7][5] = "잔뜩 갖고 올게~…….";
