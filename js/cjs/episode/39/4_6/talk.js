81df987ce789bf94b7706dc01a298046c5608281
uhlmkashim <sagisawa0330@gmail.com>
2021-11-01 23:21:02 +0900

    1

---
+++ js/cjs/episode/39/4_6/talk.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];﻿
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "ほな､うちが手本を見せるさか";
+window.im_cjs.t[1][2] = "い､よう見とってな｡まぁ､縄に";
+window.im_cjs.t[1][3] = "神火をいただくだけ｡難しいこ";
+window.im_cjs.t[1][4] = "とはあらへんよ｡";
+
+window.im_cjs.t[2][1] = "めらめら､ゆらゆら……清い";
+window.im_cjs.t[2][2] = "炎は､気を落ち着けてくれは";
+window.im_cjs.t[2][3] = "るなぁ｡暮れの心に､染み入る";
+window.im_cjs.t[2][4] = "光景､いうんやろか｡";
+
+window.im_cjs.t[3][1] = "ﾌﾟﾛﾃﾞｭｰｻｰはんは､ずっと休む";
+window.im_cjs.t[3][2] = "暇がなかったんとちゃいます? ";
+window.im_cjs.t[3][3] = "この一年､お疲れさんどした｡";
+window.im_cjs.t[3][4] = "それから､おおきに｡";
+
+window.im_cjs.t[4][1] = "ﾌﾟﾛﾃﾞｭｰｻｰはんが､いつも寄り";
+window.im_cjs.t[4][2] = "添ってくれはるさかい……";
+window.im_cjs.t[4][3] = "うちは､あいどるの舞台で､";
+window.im_cjs.t[4][4] = "のびのびと舞えるんどす｡";
+
+window.im_cjs.t[5][1] = "感謝しても､しきれんよ｡それ";
+window.im_cjs.t[5][2] = "に……年の瀬まで､ﾌﾟﾛﾃﾞｭｰｻｰ";
+window.im_cjs.t[5][3] = "はんの時間を独り占め｡うち､";
+window.im_cjs.t[5][4] = "贅沢者やなぁ｡ふふふっ♪";
+
+window.im_cjs.t[6][1] = "ﾌﾟﾛﾃﾞｭｰｻｰはんと時を重ねる";
+window.im_cjs.t[6][2] = "だけで､幸せはどんどん大き";
+window.im_cjs.t[6][3] = "なってます｡そんで､うちは……";
+window.im_cjs.t[6][4] = "どんどん貪欲に……｡";
+
+window.im_cjs.t[7][1] = "……そんなうちでも､傍に";
+window.im_cjs.t[7][2] = "いてくれはる? 来年も､";
+window.im_cjs.t[7][3] = "その先も……｡";
+window.im_cjs.t[7][4] = "ふふっ､すこ～しだけ､";
+window.im_cjs.t[7][5] = "いけずな質問やったやろか♪";
