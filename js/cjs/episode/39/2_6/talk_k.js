0cd0518971742dafb527492532a85eeab55f9003
uhlmkashim <sagisawa0330@gmail.com>
2021-11-02 20:18:36 +0900

    1

---
+++ js/cjs/episode/39/2_6/talk_k.js
@@ -0,0 +1,42 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];﻿
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//______________________"１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "허허, 이거 굉장해! 멋진 조형이군요.";
+window.im_cjs.t[1][2] = "도색도 훌륭해!";
+window.im_cjs.t[1][3] = "보는 것만으로도, 가슴이 뛰어!";
+window.im_cjs.t[1][4] = "온 보람이 있었습니다!";
+
+window.im_cjs.t[2][1] = "인터넷에서도 살 수 있는 시대지만,";
+window.im_cjs.t[2][2] = "역시 실물이 좋아! 게다가,";
+window.im_cjs.t[2][3] = "다들 한결같이 기쁜 얼굴로……";
+window.im_cjs.t[2][4] = "크리스마스는, 정말 좋은 겁니다!";
+
+window.im_cjs.t[3][1] = "자신의 어릴 적이 생각납니다…….";
+window.im_cjs.t[3][2] = "장난감 가게에 오면, 프라모델";
+window.im_cjs.t[3][3] = "코너로 직행하고…… 앗, 지금도";
+window.im_cjs.t[3][4] = "마찬가지였습니다!";
+
+window.im_cjs.t[4][1] = "저는 어른이 되었습니다만,";
+window.im_cjs.t[4][2] = "장난감 가게는 예나 지금이나";
+window.im_cjs.t[4][3] = "변함없이, 모두에게 즐거운 장소.";
+window.im_cjs.t[4][4] = "좋은 추억의 장소입니다!";
+
+window.im_cjs.t[5][1] = "생일 선물을 졸랐던 장소,";
+window.im_cjs.t[5][2] = "오빠와 갖고 싶은 장난감에 대해";
+window.im_cjs.t[5][3] = "다퉜던 장소, 크리스마스 선물을";
+window.im_cjs.t[5][4] = "사주셨던 장소…….";
+
+window.im_cjs.t[6][1] = "그렇지, 프로듀서 님! 괜찮으시다면,";
+window.im_cjs.t[6][2] = "저와 프라모델 만들어보시겠습니까?";
+window.im_cjs.t[6][3] = "하나의 프라모델을 협력해, 만든다!";
+window.im_cjs.t[6][4] = "크리스마스 결전, 입니다!";
+
+window.im_cjs.t[7][1] = "음, 실로 좋은 크리스마스입니다!";
+window.im_cjs.t[7][2] = "게다가, 프로듀서 님과 함께 ";
+window.im_cjs.t[7][3] = "프라모델을 즐길 수 있다면……";
+window.im_cjs.t[7][4] = "더 좋아하게 될지도";
+window.im_cjs.t[7][5] = "모릅니다♪";
