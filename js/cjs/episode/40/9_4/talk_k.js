5b753f69f9a0c973a5594e8a22c612236c7b10ac
uhlmkashim <sagisawa0330@gmail.com>
2021-11-03 00:13:24 +0900

    1

---
+++ js/cjs/episode/40/9_4/talk_k.js
@@ -0,0 +1,48 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "……아, 기다려줘, 프로듀서.";
+window.im_cjs.t[1][2] = "다음으로 구두랑 글러브를 끼면,";
+window.im_cjs.t[1][3] = "예쁜 신부 완성! 어때어때? 예뻐?";
+window.im_cjs.t[1][4] = "기대 이상? 그치~♪";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[2][1] = "웨딩드레스는, 마음이 긴장된다고";
+window.im_cjs.t[2][2] = "할까, 등줄기가 펴진다고 할까……";
+window.im_cjs.t[2][3] = "조금 긴장되네.";
+window.im_cjs.t[2][4] = "몇번을 입어도.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[3][1] = "물론 캣츠의 유니폼을 입을 때도,";
+window.im_cjs.t[3][2] = "되게 마음이 긴장돼!";
+window.im_cjs.t[3][3] = "마구 기합 들어가고,";
+window.im_cjs.t[3][4] = "질 수 없다! 란 느낌.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[4][1] = "하지만 역시, 그거랑은 다른";
+window.im_cjs.t[4][2] = "거잖아? 쏟는 정열은, 비슷할지도";
+window.im_cjs.t[4][3] = "모르지만, 결혼식이란,";
+window.im_cjs.t[4][4] = "역시 특별한걸!";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[5][1] = "그러니까, 프로듀서, 오늘은";
+window.im_cjs.t[5][2] = "날 그런 기분 들게 하면 안돼?";
+window.im_cjs.t[5][3] = "캣츠 얘기는, 잘자! 지금은,";
+window.im_cjs.t[5][4] = "예쁜 신부 유키니까.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[6][1] = "얘기하는 건, 앞으로 매일";
+window.im_cjs.t[6][2] = "어떻게 보낼까나, 서로 되고픈";
+window.im_cjs.t[6][3] = "이상의 모습이나, 함께 이루고픈";
+window.im_cjs.t[6][4] = "꿈이나…… 그런, 미래의 얘기!";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[7][1] = "오늘은 아직…… 아니, 앞으로도";
+window.im_cjs.t[7][2] = "시간은 충분히 있으니까,";
+window.im_cjs.t[7][3] = "좀더 많이 얘기하자!";
+window.im_cjs.t[7][4] = "프로듀서는,";
+window.im_cjs.t[7][5] = "내 보좌역이니까♪";
