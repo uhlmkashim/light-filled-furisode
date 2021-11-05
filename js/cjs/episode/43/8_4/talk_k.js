ae78fbc119f7d34975f74f96451de6a09ba5f296
uhlmkashim <sagisawa0330@gmail.com>
2021-11-05 23:28:35 +0900

    1

---
+++ js/cjs/episode/43/8_4/talk_k.js
@@ -0,0 +1,52 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "프로듀서 씨. 어딜 보고 있어?";
+window.im_cjs.t[1][2] = "……아아, 다리가 너무 눈에 띄어?";
+window.im_cjs.t[1][3] = "걱정할 것 없어, 제대로 아래에 입고 있는걸.";
+window.im_cjs.t[1][4] = "";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[2][1] = "조금 기대했어? 라니,";
+window.im_cjs.t[2][2] = "조금은 동요해주지 않으면 재미없어.";
+window.im_cjs.t[2][3] = "혹시 익숙해진 거라면,";
+window.im_cjs.t[2][4] = "다른 전략도 생각하지 않으면 안 되겠네.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[3][1] = "후후, 나쁜 계략은 이쯤 하고……";
+window.im_cjs.t[3][2] = "호의를 받아들여서, 족탕을 만끽하도록 할게.";
+window.im_cjs.t[3][3] = "후우…… 기분 좋아.";
+window.im_cjs.t[3][4] = "";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[4][1] = "긴장됐던 몸이, 풀어지는 것 같아.";
+window.im_cjs.t[4][2] = "피로는 남기지 않는 주의지만,";
+window.im_cjs.t[4][3] = "모르는 사이에 쌓여있었는지도.";
+window.im_cjs.t[4][4] = "";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[5][1] = "그걸 헤아리고 여기에 데려오다니,";
+window.im_cjs.t[5][2] = "역시 내 뒷면의 뒷면까지 간파당한 걸까?";
+window.im_cjs.t[5][3] = "당신의 그 눈동자에……♪";
+window.im_cjs.t[5][4] = "";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[6][1] = "후후. 알려준 효능, 정말이었던 것 같아.";
+window.im_cjs.t[6][2] = "왜냐면 봐,";
+window.im_cjs.t[6][3] = "벌써 얼굴이 붉어지고 있어.";
+window.im_cjs.t[6][4] = "당신도 나도…… 농담이야.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[7][1] = "따뜻해지면, 다른 곳도 가보고 싶어.";
+window.im_cjs.t[7][2] = "체크하고 있지,";
+window.im_cjs.t[7][3] = "내가 좋아할 만한 장소.";
+window.im_cjs.t[7][4] = "어디든 어울려줄게.";
+window.im_cjs.t[7][5] = "그야말로, 뒷면의 뒷면까지♪";
+
+
+
+
