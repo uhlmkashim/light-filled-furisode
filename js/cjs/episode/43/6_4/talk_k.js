77254ed05ba081f04bccdbb3814ff73559ab6fc3
uhlmkashim <sagisawa0330@gmail.com>
2021-11-06 00:47:21 +0900

    1

---
+++ js/cjs/episode/43/6_4/talk_k.js
@@ -0,0 +1,51 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "고백…… 고, 오, 배, 액……. 그게";
+window.im_cjs.t[1][2] = "그러니까, 즉, 전하고 싶은 거!";
+window.im_cjs.t[1][3] = "어~떡해~! P 쨩,";
+window.im_cjs.t[1][4] = "슬슬 올 때쯤일까늬~?";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[2][1] = "「학교에서 제일, 큰 나무 아래에서,";
+window.im_cjs.t[2][2] = "고백이 성공하면, 두사람은 영원히 행복해질";
+window.im_cjs.t[2][3] = "수 있다」라니…… 우꺄~!";
+window.im_cjs.t[2][4] = "너무 두근두근~해앵!";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[3][1] = "뇨와와, P 쨩 어느새에~!";
+window.im_cjs.t[3][2] = "수고수고! ……이, 일하기 전에,";
+window.im_cjs.t[3][3] = "키라링에게서 중요한 얘기,";
+window.im_cjs.t[3][4] = "들어주지 않을래앵?";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[4][1] = "저기, P 쨩은, 키라리를 아이돌로 만들어줘서,";
+window.im_cjs.t[4][2] = "해피해피를 잔뜩 줬잖아.";
+window.im_cjs.t[4][3] = "그러니까, 키라리도 잔뜩 보답하고 싶어!";
+window.im_cjs.t[4][4] = "";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[5][1] = "그, 그걸 위해서, P 쨩에겐……";
+window.im_cjs.t[5][2] = "계속, 계~속! 키라리 곁에서,";
+window.im_cjs.t[5][3] = "P 쨩으로 있어줬으면 좋겠어요!";
+window.im_cjs.t[5][4] = "……안될, 까?";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[6][1] = "엣, 괜찮은 거늬? P 쨩,";
+window.im_cjs.t[6][2] = "진짜진짜로 괜찮아~?";
+window.im_cjs.t[6][3] = "우헤헤, 키라리랑 P 쨩,";
+window.im_cjs.t[6][4] = "같은 기분이었구나☆";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[7][1] = "있잖아, P 쨩. 키라리는 말야~,";
+window.im_cjs.t[7][2] = "어제도 오늘도 내일도,";
+window.im_cjs.t[7][3] = "P쨩이 있어 준다면,";
+window.im_cjs.t[7][4] = "쭉~ 행복할 거다늬♪";
+
+
+
+
