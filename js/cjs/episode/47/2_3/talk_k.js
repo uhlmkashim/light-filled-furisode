6c0aeeb62c3a9e079bb0f24f0877c9729967e6d7
uhlmkashim <sagisawa0330@gmail.com>
2022-08-09 21:40:41 +0900

    1

---
+++ js/cjs/episode/47/2_3/talk_k.js
@@ -0,0 +1,48 @@
+window.im_cjs = window.im_cjs || {};
+window.im_cjs.t = [10];
+for (var i = 0, len = 10; i < len; i++) {
+	window.im_cjs.t[i] = [];
+}
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[1][1] = "그럼, 이쯤에서 잠깐 쉬었다 ";
+window.im_cjs.t[1][2] = "가자구. 나도 너도 촬영하느라 ";
+window.im_cjs.t[1][3] = "피곤하고, 마침 전하고 싶은 것도 ";
+window.im_cjs.t[1][4] = "있고 하니까-.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[2][1] = "대충 검토는 되고 있겠지. ";
+window.im_cjs.t[2][2] = "그럼 짠-! 시키쨩의 수제 ";
+window.im_cjs.t[2][3] = "초콜릿~. 어때, 기쁘지? ";
+window.im_cjs.t[2][4] = "기뻐하라구-♪";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[3][1] = "아, 그 표정. 이상한 약이라도 ";
+window.im_cjs.t[3][2] = "넣은게 아닌가 생각 중이지. ";
+window.im_cjs.t[3][3] = "너무한걸-. 이렇게나 좋은 냄새가 ";
+window.im_cjs.t[3][4] = "나는데……킁킁…………냠.";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[4][1] = "냐하, 못 참고 먹어버렸다♪ ";
+window.im_cjs.t[4][2] = "그래도 이걸로, 안전은 보장되었네-. ";
+window.im_cjs.t[4][3] = "응~, 입안에서 사르르 녹는 와중에, ";
+window.im_cjs.t[4][4] = "확실한 달콤함♪";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[5][1] = "자, 마음에 드는걸 골라봐. ";
+window.im_cjs.t[5][2] = "선택의 역설 문제는 일어나지 ";
+window.im_cjs.t[5][3] = "않아. 그야, 이거 전부, 네가 ";
+window.im_cjs.t[5][4] = "먹어도 되니까♪";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[6][1] = "후훗, 먹었네-♪ 너, 최근에 ";
+window.im_cjs.t[6][2] = "과로했으니까. 내 초코는-……";
+window.im_cjs.t[6][3] = "굉장히, 효과가 있을거야.";
+window.im_cjs.t[6][4] = "";
+
+//                      "１２３４５６７８９０１２３４5"
+window.im_cjs.t[7][1] = "그럼 여기서 스포일러를. 사실은 ";
+window.im_cjs.t[7][2] = "하나에만, 수제 약이 들어있어. ";
+window.im_cjs.t[7][3] = "효과는 지금, 확인해봐. ";
+window.im_cjs.t[7][4] = "……있잖아, 너의 눈동자에, ";
+window.im_cjs.t[7][5] = "나는 어떻게 비치고 있어?";
