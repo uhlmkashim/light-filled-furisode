cd64f9d9737eeafe6ba58c4090264021aa3b01e3
uhlmkashim <sagisawa0330@gmail.com>
2022-08-15 16:05:00 +0900

    1

--- js/cjs/episode/49/0_2/talk_k.js
+++ js/cjs/episode/49/0_2/talk_k.js
@@ -6,9 +6,9 @@ for (var i = 0, len = 10; i < len; i++) {
 
 //                      "１２３４５６７８９０１２３４5"
 window.im_cjs.t[1][1] = "어떠세요? 프로듀서 쨔마.";
-window.im_cjs.t[1][2] = "바다가 예쁘죠? 여긴 사쿠라이의";
-window.im_cjs.t[1][3] = "별장 중에서도 손꼽히는";
-window.im_cjs.t[1][4] = "피서지예요♪";
+window.im_cjs.t[1][2] = "바다가 예쁘죠? 여긴 사쿠라이의 별장";
+window.im_cjs.t[1][3] = "중에서도 손꼽히는 피서지예요♪";
+window.im_cjs.t[1][4] = "";
 
 //                      "１２３４５６７８９０１２３４5"
 window.im_cjs.t[2][1] = "희미하게 들리는 파도 소리에";
@@ -17,31 +17,31 @@ window.im_cjs.t[2][3] = "시원한 풀에서 몸을 움직이거나……";
 window.im_cjs.t[2][4] = "뭘 해도 자유예요♪";
 
 //                      "１２３４５６７８９０１２３４5"
-window.im_cjs.t[3][1] = "PC도 휴대전화도, 지금만큼은";
-window.im_cjs.t[3][2] = "금지예요♪ 아빠도, 여기에 오면";
-window.im_cjs.t[3][3] = "일을 잊는다고, 자주";
-window.im_cjs.t[3][4] = "말씀하세요.";
+window.im_cjs.t[3][1] = "PC도 휴대전화도, 지금만큼은 금지예요♪";
+window.im_cjs.t[3][2] = "아빠도, 여기에 오면 일을 잊는다고,";
+window.im_cjs.t[3][3] = "자주 말씀하세요.";
+window.im_cjs.t[3][4] = "";
 
 //                      "１２３４５６７８９０１２３４5"
-window.im_cjs.t[4][1] = "항상 사람에 둘러싸여, 바쁘게";
-window.im_cjs.t[4][2] = "지내고 계시니까…… 쉬는 동안";
-window.im_cjs.t[4][3] = "정도는, 저와 둘이서,";
-window.im_cjs.t[4][4] = "느긋하게 지내요.";
+window.im_cjs.t[4][1] = "항상 사람에 둘러싸여, 바쁘게 지내고";
+window.im_cjs.t[4][2] = "계시니까…… 쉬는 동안 정도는,";
+window.im_cjs.t[4][3] = "저와 둘이서, 느긋하게 지내요.";
+window.im_cjs.t[4][4] = "";
 
 //                      "１２３４５６７８９０１２３４5"
-window.im_cjs.t[5][1] = "그래요. 저녁에, 시원해지면";
-window.im_cjs.t[5][2] = "바닷가를 산책할까요♪";
-window.im_cjs.t[5][3] = "프라이트 비치니까, 불꽃놀이를";
-window.im_cjs.t[5][4] = "하는 것도 좋겠네요.";
+window.im_cjs.t[5][1] = "그래요. 저녁에, 시원해지면 바닷가를";
+window.im_cjs.t[5][2] = "산책할까요♪";
+window.im_cjs.t[5][3] = "프라이트 비치니까, 불꽃놀이를 하는 것도";
+window.im_cjs.t[5][4] = "좋겠네요.";
 
 //                      "１２３４５６７８９０１２３４5"
-window.im_cjs.t[6][1] = "어머…… 저 좀 봐, 그만";
-window.im_cjs.t[6][2] = "들떠버렸네요. 프로듀서";
-window.im_cjs.t[6][3] = "쨔마와 하고 싶은 게,";
-window.im_cjs.t[6][4] = "자꾸 떠올라 버려서요.";
+window.im_cjs.t[6][1] = "어머…… 저 좀 봐, 그만 들떠버렸네요.";
+window.im_cjs.t[6][2] = "프로듀서 쨔마와 하고 싶은 게, 자꾸";
+window.im_cjs.t[6][3] = "떠올라 버려서요.";
+window.im_cjs.t[6][4] = "";
 
 //                      "１２３４５６７８９０１２３４5"
-window.im_cjs.t[7][1] = "프로듀서 쨔마도, 하고 싶은";
-window.im_cjs.t[7][2] = "게 있다면 뭐든지 말해주시길.";
+window.im_cjs.t[7][1] = "프로듀서 쨔마도, 하고 싶은 게 있다면";
+window.im_cjs.t[7][2] = "뭐든지 말해주시길.";
 window.im_cjs.t[7][3] = "당신의 응석이라면,";
 window.im_cjs.t[7][4] = "저 대환영인걸요♪";
